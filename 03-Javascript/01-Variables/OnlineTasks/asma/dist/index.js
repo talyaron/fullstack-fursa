3b162506d846d188f8a41f6e1fe8c997f8b07bfc
asmaafraij <asmaafraij@gmail.com>
2022-02-06 11:34:29 +0200

    asma

---
+++ 03-Javascript/01-Variables/OnlineTasks/asma/dist/index.js
@@ -0,0 +1,2 @@
+var obj = { name: "asmaa", email: "asmaafraij@gmail.com", age: 25, phonenumber: "0527408690" };
+console.log(obj);
---
+++ 03-Javascript/03-loops/OnlineTasks/asmaa/dist/index.js
@@ -0,0 +1,4 @@
+var obj = { name: "asmaa", email: "asmaafraij@gmail.com", age: 25, phonenumber: "0527408690" };
+for (var val in obj) {
+    console.log(val, ":", obj[val]);
+}
