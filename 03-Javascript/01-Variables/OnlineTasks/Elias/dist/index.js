a89bf4352eb830a080a6d18dd7c279804d6f0ec4
Elias Re <eliasrenawi23@gmail.com>
2022-02-06 11:36:38 +0200

    elias

---
+++ 03-Javascript/01-Variables/OnlineTasks/Elias/dist/index.js
@@ -0,0 +1,10 @@
+var profile = {
+    name: "Elias",
+    Email: null,
+    age: null,
+    phonenumber: "0528423617"
+};
+console.log(profile);
+profile.Email = "eliasrenawi23@gmail.com";
+profile.age = 24;
+console.log(profile);
