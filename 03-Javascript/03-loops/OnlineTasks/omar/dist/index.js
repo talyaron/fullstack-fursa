833f97e447d0d4bb816ea6367613c64a503ecbc2
omarhallomi <omarhallomi7@gmail.com>
2022-02-06 11:44:52 +0200

    aa

--- 03-Javascript/03-loops/OnlineTasks/omar/dist/index.js
+++ 03-Javascript/03-loops/OnlineTasks/omar/dist/index.js
@@ -5,7 +5,6 @@ var details = {
     phone: 9658,
     work: "software enginer"
 };
-for (var _i = 0, details_1 = details; _i < details_1.length; _i++) {
-    var elm = details_1[_i];
-    console.log(elm);
+for (var key in details) {
+    console.log(details[key]);
 }
