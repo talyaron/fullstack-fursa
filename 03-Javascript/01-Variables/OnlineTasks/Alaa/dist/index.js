ecf147608078dae11d1c81c82cc0e6f8548a33b9
AlaaAga <alaa.ali.aga1@gmail.com>
2022-02-06 10:42:24 +0200

    AlaaOnLineTask

---
+++ 03-Javascript/01-Variables/OnlineTasks/Alaa/dist/index.js
@@ -0,0 +1,6 @@
+//typescript is js with types
+//primitive types
+var obj = { Name: "Alaa Abu raya", email: "Alaa.ali.aga1@gmail.com", birthDay: new Date("1995-09-09"), phoneNumber: "0505388550" };
+obj.Name = "Abo Ali";
+// obj.email = "Alaa@gmail.com";
+console.log(obj);
---
+++ 03-Javascript/01-Variables/OnlineTasks/Alaa/index.ts
@@ -0,0 +1,19 @@
+//typescript is js with types
+//primitive types
+
+
+
+
+//objects
+interface person {
+    Name: string;
+    email: string;
+    birthDay: Date;
+    phoneNumber: string;
+
+}
+
+const obj:person = { Name:"Alaa Abu raya",email: "Alaa.ali.aga1@gmail.com" , birthDay: new Date("1995-09-09") , phoneNumber:"0505388550"};
+obj.Name = "Abo Ali";
+// obj.email = "Alaa@gmail.com";
+console.log(obj);
