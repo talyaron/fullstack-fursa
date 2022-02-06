b152f5e570b4e2618757399a918fc547d0dce141
Yosef <Yossefzoubi@gmail.com>
2022-02-06 11:36:05 +0200

    yosef

---
+++ 03-Javascript/03-loops/Yosef/dist/index.js
@@ -0,0 +1,4 @@
+var obj = { name: 'Yosef', LastName: 'Zoubi',
+    Age: 25, location: 'Nazareth', Hobbies: 'online games' };
+for (var key in obj)
+    console.log(key, ':', obj[key]);
---
+++ 03-Javascript/03-loops/Yosef/index.ts
@@ -0,0 +1,5 @@
+const obj = {name : 'Yosef', LastName : 'Zoubi',
+Age : 25, location : 'Nazareth', Hobbies : 'online games'}
+
+for(let key in obj)
+    console.log(key, ':', obj[key]);
