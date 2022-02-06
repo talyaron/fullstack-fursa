9f2bdc0a5d80b430a4c5f762439b82a9f1f50598
aimanyounsies1 <aymanyounis@icloud.com>
2022-02-06 10:37:06 +0200

    added

---
+++ 03-Javascript/01-Variables/OnlineTasks/Aiman/index.ts
@@ -0,0 +1,16 @@
+interface details {
+  age: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+const aiman:details = {
+  age: 25,
+  name: "Aiman Younis",
+  email: "aimanyounises@gmail.com",
+  phone: "+972544991012",
+};
+
+document.getElementById('details').innerHTML = aiman.age+  " " + aiman.email +  " " + aiman.name +  " " + aiman.phone;
+console.log(aiman)
