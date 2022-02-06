5f6c1bef062aeaf2c85fb23be09bead94816857d
aimanyounsies1 <aymanyounis@icloud.com>
2022-02-06 11:41:13 +0200

    added:2

--- 03-Javascript/03-loops/OnlineTasks/Aiman/dist/index.js
+++ 03-Javascript/03-loops/OnlineTasks/Aiman/dist/index.js
@@ -8,7 +8,7 @@ console.log(aiman);
 for (var index in aiman) {
     console.log(aiman[index]);
     var element = document.createElement('div');
-    element.style.cssText = 'width:100% ; height :10vh  ; font-weight:bold; ; font-size:3em';
+    element.style.cssText = 'width:100% ; height :10vh  ; font-weight:bold; ;font-size:3em';
     element.innerHTML = index + ": " + aiman[index];
     document.body.appendChild(element);
 }
