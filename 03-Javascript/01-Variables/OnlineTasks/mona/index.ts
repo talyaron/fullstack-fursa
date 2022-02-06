920e60b6f1eaf09b4106167541b8c4610de3933a
Mona <mona_arabiya@hotmail.com>
2022-02-06 11:36:20 +0200

    update

---
+++ 03-Javascript/01-Variables/OnlineTasks/mona/index.ts
@@ -0,0 +1,10 @@
+interface profile{
+    name:string;
+    email:string;
+    age:number;
+    phonenumber:string;
+
+}
+const profobj: profile={name:'mona',email:'mona_arabiya@hotmail.com',age:25,phonenumber:'0502804839'};
+console.log(profobj)
+
