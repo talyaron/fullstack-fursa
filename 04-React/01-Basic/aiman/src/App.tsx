1ef9f6749819deca25b96d0f164f90013c9473ba
aimanyounises1 <aymanyounis@icloud.com>
2022-02-07 22:44:11 +0200

    finished last online_task

--- 04-React/01-Basic/aiman/src/App.tsx
+++ 04-React/01-Basic/aiman/src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import Card from "./components/views/Card";
+
+interface Cards {
+  title: string;
+  src: string;
+}
+
+const arr: Array<Cards> = [
+  {
+    title: "Nature",
+    src: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
+  },
+
+  {
+    title: "Liverpool",
+    src: "https://imageio.forbes.com/specials-images/imageserve/1153850565/Liverpool-Parade-to-Celebrate-Winning-UEFA-Champions-League/960x0.jpg?fit=bounds&format=jpg&width=960",
+  },
+];
+const hello_world = "Hello World";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {arr.map((card , index)=>{
+          return <Card key={index} src={card.src} title={card.title} />
+        })}
       </header>
     </div>
   );
