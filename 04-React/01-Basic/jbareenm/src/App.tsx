9d67bf68b42dc5d3ad0ccec91ca8ebd90396397e
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-02-07 12:55:23 +0200

    changes

--- 04-React/01-Basic/jbareenm/src/App.tsx
+++ 04-React/01-Basic/jbareenm/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './dist/App.css';
 
 function App() {
+  const clicked = 10;
+  function handleSubmit(e:any) {
+    e.preventDefault();
+    alert('You clicked submit.');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>number of times clicked {clicked}</p>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
