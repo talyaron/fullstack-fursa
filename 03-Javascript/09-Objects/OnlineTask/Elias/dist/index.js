769252021d82bb33bd2f1cda027c535f041f5ebd
Elias Re <eliasrenawi23@gmail.com>
2022-02-07 11:12:25 +0200

    Elias_mona

---
+++ 03-Javascript/09-Objects/OnlineTask/Elias/dist/index.js
@@ -0,0 +1,25 @@
+var teams;
+(function (teams) {
+    teams[teams["SACHNIN"] = 0] = "SACHNIN";
+    teams[teams["BEITAR"] = 1] = "BEITAR";
+    teams[teams["keryat_shmona"] = 2] = "keryat_shmona";
+})(teams || (teams = {}));
+var schaninBitarJs = {
+    date: new Date("02-13-2022"),
+    team1: teams.BEITAR,
+    team2: teams.SACHNIN,
+    winner: undefined,
+    setWinner: function (winner) {
+        try {
+            if (winner !== this.team1 && winner !== this.team2)
+                throw new Error("winner is not one of the compiting teams");
+            this.winner = winner;
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+};
+console.log(schaninBitarJs);
+schaninBitarJs.setWinner(teams.BEITAR);
+console.log(schaninBitarJs);
