2710ac017aaed68e1a2d2474bfa9518c9ab6c1b5
omarhallomi <omarhallomi7@gmail.com>
2022-02-08 21:44:43 +0200

    newOmar

--- 04-React/01-Basic/omar-app/src/App.tsx
+++ 04-React/01-Basic/omar-app/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+//components
+import Exercise from './view/components/exercise/Exercise';
 
+const members = [{name:'Saleem', part:'chest'},{name:'Omar',part:"Legs"}];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {members.map((member, index)=>{
+        return <Exercise key={index} name={member.name} part={member.part} />
+      })}
       </header>
     </div>
-    
   );
 }
 
