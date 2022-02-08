3cb8effd76ea7948341c743f60413b587be3cb6c
DimaAbbas <dimaabbas25@gmail.com>
2022-02-08 21:53:03 +0200

    update

--- 04-React/01-Basic/dima/src/App.tsx
+++ 04-React/01-Basic/dima/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+//components
+import Card from './view/components/card/Card';
+
+const imagesArr:Array<image> = [
+  {link:"https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg",title:"image 1"},
+  {link:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", title:"image 2"}];
+interface image{
+  link:string;
+  title:string;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>My name is Dima</p>
-        <p>Hiiiiii</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {imagesArr.map((image, index) =>
+         {return <Card key={index} link={image.link} title={image.title}/>})}
       </header>
     </div>
   );
