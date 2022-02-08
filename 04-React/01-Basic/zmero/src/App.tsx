324ee689e29883abc2318fe37eb72756823a7b23
mzmero <m.zmiro@hotmail.com>
2022-02-08 20:04:59 +0200

    project first page

--- 04-React/01-Basic/zmero/src/App.tsx
+++ 04-React/01-Basic/zmero/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-const counter = 35;
+import './App.scss';
+
+import Navbar from './view/components/menu/menu';
+import SearchBar from './view/components/search/search';
+import Explore from './view/components/explore/explore';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>{counter}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar></Navbar>
       </header>
+
+      <SearchBar></SearchBar>
+      <Explore></Explore>
     </div>
   );
 }
