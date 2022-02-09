aa2e1a76d7117b01b6ba25f087f2b73e7d631816
Suzan Kassabry <suzankassabry97@gmail.com>
2022-02-09 17:30:53 +0200

    personal project

---
+++ 98-Private-projects/suzan/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+//components
+import Card from './view/components/card/Card';
+import MyCard from './view/components/card/Card';
+import { title } from 'process';
+
+// const students:Array<Place> = [{name:'Saleem', place:'Mashad'},{name:'Mona',place:"Nazereth"}];
+// interface Place{
+//   name:string;
+//   place:string
+// }
+
+interface Box{
+  image: string;
+  title: string;
+}
+
+const box = {image:'https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg', title:'Hello I am a title'};
+const boxes:Array<Box> = [box, box, box, box];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      {boxes.map((box1, index)=>{
+        const {image, title} = box1
+        return <MyCard key={index} info = {box1} />
+      })
+        // <MyCard image={box.image} title={box.title} />
+      /* {students.map((student, index)=>{
+        return <Card key={index} title={student.name} place={student.place} />
+      })} */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
