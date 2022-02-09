8a5d50936ca4a3447bffce28989f0bd0f89ac3be
DimaAbbas <dimaabbas25@gmail.com>
2022-02-09 12:05:48 +0200

    change color

--- 04-React/01-Basic/dima/src/view/components/card/Card.tsx
+++ 04-React/01-Basic/dima/src/view/components/card/Card.tsx
@@ -1,16 +1,31 @@
-interface CardProp{
-    info:{
-        link:string;
-        title:string;
+import { useState } from "react";
+
+interface CardProp {
+    info: {
+        link: string;
+        title: string;
     }
 }
 
-function Card(prop:CardProp) {
-    const {link,title} = prop.info;
-    return(
+function Card(prop: CardProp) {
+    const { link, title } = prop.info;
+    
+    const [color, setColor] = useState("#385c78");
+
+    function HandleClick(ev: any) {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        setColor(color);
+    }
+
+    return (
         <div className="card">
-            <img src={link}/>
+            <img src={link} />
             <h3>{title}</h3>
+            <button onClick={HandleClick} style={{backgroundColor: color}}>Change color</button>
         </div>
     );
 }
