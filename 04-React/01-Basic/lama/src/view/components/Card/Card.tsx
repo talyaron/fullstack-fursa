16d0bfa716b5f5dbd0dc9e73562a107e76ff5e4a
lama.murad.lm@gmail.com <lama.murad.lm@gmail.com>
2022-02-09 14:25:17 +0200

    online tasks

--- 04-React/01-Basic/lama/src/view/components/Card/Card.tsx
+++ 04-React/01-Basic/lama/src/view/components/Card/Card.tsx
@@ -15,13 +15,40 @@ interface CardProp{
 
 
 function Card(prop:CardProp){
+    const [color,SetColor]=useState('red');
+    const [names,Setname]=useState("Lama");
+    const names2=["a","b","c","d","E","f"];
     const {name,age,height,img}=prop.info;
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        SetColor(color);
+    }
+    function changename(e: any) {
+        e.preventDefault();
+         var nameInArr = names2[Math.floor(Math.random()*names2.length)];
+
+        Setname(nameInArr);
+    }
+
     return(
-        <div className="card">
+        <div className="card" style={{backgroundColor:color}}>
             <img src="{img}" alt="" />
             <h3>{name}</h3>
+            <h1>{names}</h1>
             <p>age is: {age}</p>
             <p>height is: {height}</p>
+            <form onSubmit={changename}>
+                <button className='btn' type="submit">change name</button>
+            </form>
+            <form onSubmit={handleSubmit}>
+                <button className='btn' type="submit">change color</button>
+            </form>
         </div>
     );
 }
