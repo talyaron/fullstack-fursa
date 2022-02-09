b6ba3b2b31a13a283436a41e1f1474d322cfdc0b
Tal Yaron <tal.yaron@gmail.com>
2022-02-09 11:31:04 +0200

    useState

--- 04-React/01-Basic/example/src/view/components/card/Card.tsx
+++ 04-React/01-Basic/example/src/view/components/card/Card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface CardProp {
   info: {
     name: string;
@@ -7,13 +9,33 @@ interface CardProp {
 }
 
 function Card(prop: CardProp) {
-  const {name, place, img} = prop.info;
-  
+  const { name, place, img } = prop.info;
+
+  //state
+  const [counter, setCounter] = useState(0); // useState(initial value);
+  let counter2 = 0;
+
+  function handleAddCounter() {
+    let tempCounter = counter;
+    console.log('before:',tempCounter);
+    tempCounter++;
+   
+    setCounter(tempCounter);
+    console.log('after:',tempCounter);
+  }
+
+  function handleAddCounter2(){
+    counter2++;
+    console.log(counter2)
+  }
+
   return (
     <div className="card">
       <img src={img} alt={place} />
       <h3>{name}</h3>
       <p>Lives in: {place}</p>
+      <button onClick={handleAddCounter}>{counter}</button>
+      <button onClick={handleAddCounter2}>Counter2: {counter2}</button>
     </div>
   );
 }
