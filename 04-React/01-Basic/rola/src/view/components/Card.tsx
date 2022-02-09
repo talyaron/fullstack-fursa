6b3970c16c85e96d972e69d6f4373f5b8e832e05
RolaMarei <rola.marei.34@hotmail.com>
2022-02-09 12:44:01 +0200

    colors

--- 04-React/01-Basic/rola/src/view/components/Card.tsx
+++ 04-React/01-Basic/rola/src/view/components/Card.tsx
@@ -1,14 +1,53 @@
 import "../components/Card.scss";
 
-interface Cardprob{
-    title:string;
-    img:string;
+// function Card(prob:Cardprob) {
+//   return <div className="card"> 
+//       <img src={prob.img}  />
+//       <p> {prob.title} </p>
+//   </div>;
+// }
+
+// export default Card;
+
+import { useState } from "react";
+
+interface CardProp {
+  info: {
+      name: string;
+      place: string;
+      img: string;
+  };
 }
-function Card(prob:Cardprob) {
-  return <div className="card"> 
-      <img src={prob.img}  />
-      <p> {prob.title} </p>
-  </div>;
+
+function Card(prop: CardProp) {
+  const { name, place, img } = prop.info;
+
+  //state
+  const [color, setcolor] = useState('yellow'); // useState(initial value);
+ // let counter2 = 0;
+
+  function handlcolor() {
+    var letters = '0123456789ABCDEF';
+    let newColor:string = '#';
+    for (var i = 0; i < 6; i++) {
+        newColor += letters[Math.floor(Math.random() * 16)];
+    }
+    setcolor(newColor);
+  }
+
+  // function handleAddCounter2(){
+  //   counter2++;
+  //   console.log(counter2)
+  // }
+console.log(color);
+  return (
+    <div className="card">
+            <h3 style={{backgroundColor:'red'}}></h3>
+      <button onClick={handlcolor} style={{backgroundColor:color}}> hello</button>
+    </div>
+
+  );
 }
 
 export default Card;
+
