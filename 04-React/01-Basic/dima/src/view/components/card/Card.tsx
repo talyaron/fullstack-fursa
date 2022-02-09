5da538a74d233aebfe04347e303b534c8baa163f
DimaAbbas <dimaabbas25@gmail.com>
2022-02-09 10:50:26 +0200

    dima

--- 04-React/01-Basic/dima/src/view/components/card/Card.tsx
+++ 04-React/01-Basic/dima/src/view/components/card/Card.tsx
@@ -1,13 +1,16 @@
 interface CardProp{
-    link:string;
-    title:string;
+    info:{
+        link:string;
+        title:string;
+    }
 }
 
 function Card(prop:CardProp) {
+    const {link,title} = prop.info;
     return(
         <div className="card">
-            <img src={prop.link}/>
-            <h3>{prop.title}</h3>
+            <img src={link}/>
+            <h3>{title}</h3>
         </div>
     );
 }
