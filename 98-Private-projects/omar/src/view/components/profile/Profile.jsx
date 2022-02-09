6e363184c4b1a6fefea9dd0d94f71a4c0cb3607f
omarhallomi <omarhallomi7@gmail.com>
2022-02-10 00:48:48 +0200

    eventsAdded

--- 98-Private-projects/omar/src/view/components/profile/Profile.jsx
+++ 98-Private-projects/omar/src/view/components/profile/Profile.jsx
@@ -6,13 +6,33 @@ import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import LoginIcon from '@mui/icons-material/Login';
 import GoogleIcon from '@mui/icons-material/Google';
 import BarChartIcon from '@mui/icons-material/BarChart';
+import React, {useState} from "react"
+import Dialog from '../dialog/Dialog'
+
 function ProfileButton() {
+    const [open, setOpen] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
+
+    const onConfirm1 = ()=>{
+        console.log("google!");
+    }
+    const onConfirm2 = ()=>{
+        console.log("facebook!");
+    }
+
+
+
+
     return (
         <div className = 'signUpField'> 
-            <Button size="large" variant="outlined" startIcon={<GoogleIcon/>}> Sign Up with Google</Button>
+        
+
+            <Button size="large" variant="outlined"  onClick={() => setOpen(true)} startIcon={<GoogleIcon/>}> Sign Up with Google</Button>
+            <Dialog open={open} handleClose={() => setOpen(false)} title={"Warning!!"} content={"You are going to sign up with google!"} onConfirm={onConfirm1}/>
             <br></br>
             <br></br>
-            <Button size="large" variant="outlined" startIcon={<FacebookOutlinedIcon/>}> Sign Up with Facebook</Button>
+            <Button size="large" variant="outlined" onClick={() => setOpen2(true)} startIcon={<FacebookOutlinedIcon/>}> Sign Up with Facebook</Button>
+            <Dialog open={open2} handleClose={() => setOpen2(false)} title={"Warning!!"} content ={"You are going to sign up with Facebook!"} onConfirm={onConfirm2}/>
             <br></br>
             <br></br>
             <Button size="large" variant="outlined" startIcon={<LoginIcon/>}> Allready have account ? login ! </Button>
