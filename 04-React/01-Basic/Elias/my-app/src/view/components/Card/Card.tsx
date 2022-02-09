9bedf3d760b0d5c4da599b64a4e0f147265dc9bb
Elias Re <eliasrenawi23@gmail.com>
2022-02-09 14:03:34 +0200

    input Elias

--- 04-React/01-Basic/Elias/my-app/src/view/components/Card/Card.tsx
+++ 04-React/01-Basic/Elias/my-app/src/view/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import './Card.scss';
+import{useState} from'react';
 
 interface item{
     info:{
@@ -8,19 +9,49 @@ interface item{
   }
 
 function Card(props:item) {
+    const [color,SetColor]=useState("#385c78");
+    const [names,Setname2]=useState("Elias");
+    const names2=["a","b","c","d","E","f"];
+    const [inputText, setInputText] = useState("Input Text");
+
+   
     const {url,name}=props.info;
     function handleSubmit(e: any) {
         e.preventDefault();
-        alert('You clicked submit.');
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        SetColor(color);
+    }
+    function changename(e: any) {
+        e.preventDefault();
+         var nameInArr = names2[Math.floor(Math.random()*names2.length)];
+
+        Setname2(nameInArr);
+    }
+    function HandleText(ev:any){
+        try{
+            setInputText(ev.target.value);
+        }catch(err){
+            console.error(err);
+        }
     }
     return (
-        <div className="card">
-            <img className='imgHeader' src={url} />
+        <div className="card" style={{backgroundColor:color}}>
+            <img className='imgHeader' src={url}  />
             <p className='content'>
             <h2>{name}</h2>
             </p>
+            <h1>{names}</h1>
+            <div>{inputText}</div>
+            <input type="text" onInput={HandleText}/>
+            <form onSubmit={changename}>
+                <button className='buyButton' type="submit">change name</button>
+            </form>
             <form onSubmit={handleSubmit}>
-                <button className='buyButton' type="submit">Add to Cart</button>
+                <button className='buyButton' type="submit">change color</button>
             </form>
         </div>
     );
