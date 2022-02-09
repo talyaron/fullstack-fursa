9cb46d41f894acf5be561ed1c6b88bdedd2c0d2c
aimanyounises1 <aymanyounis@icloud.com>
2022-02-09 14:03:41 +0200

    added Raneen Aiman

--- 04-React/01-Basic/aiman/src/components/views/Card.tsx
+++ 04-React/01-Basic/aiman/src/components/views/Card.tsx
@@ -1,23 +1,29 @@
-import '../views/Card.scss'
+import "../views/Card.scss";
 import { isPropertySignature } from "typescript";
+import { useState } from "react";
 interface CardProp {
-
-  info : {
-    src:string;
+  info: {
+    src: string;
     title: string;
-  }
- 
+  };
 }
 
 function Card(prop: CardProp) {
-  const {src , title} = prop.info;
-  return (  
-    <div  className="card">
+  const { src, title } = prop.info;
+  const [text, setText] = useState("");
+  let tempText: string = "";
+  function handleChange(eve: any) {
+    console.log(eve.target.value);
+    tempText = eve.target.value;
+    setText(tempText);
+  }
+  return (
+    <div className="card">
       <img src={src} alt="" />
       <p> {title}</p>
+      <h1> {text} </h1>
+      <input className="input" onChange={handleChange} id="in"></input>
     </div>
-    )
-  
+  );
 }
-
 export default Card;
