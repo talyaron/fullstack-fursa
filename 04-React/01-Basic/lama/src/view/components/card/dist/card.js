b79522e8422980250cd04d534df80f45d7a817cd
saleem <sbsaleem76@gmail.com>
2022-02-09 09:27:05 +0200

    sss

--- 04-React/01-Basic/lama/src/view/components/card/dist/card.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function card() {
-    return (React.createElement("div", { className: "card" },
-        React.createElement("h2", null, "welcome to react"),
-        React.createElement("p", null, "this is a card")));
-}
-exports["default"] = card;
