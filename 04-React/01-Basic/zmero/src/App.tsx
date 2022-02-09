90de5cce58495f35cb927ad95613cc20bd532870
mzmero <m.zmiro@hotmail.com>
2022-02-09 18:22:13 +0200

    home task

--- 04-React/01-Basic/zmero/src/App.tsx
+++ 04-React/01-Basic/zmero/src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss';
 
 import Card from './view/components/card/card';
 
-const students: Array<Place> = [{ name: 'Saleem', place: 'Mashad', img: 'https://i0.wp.com/www.hoshvilim.com/wp-content/uploads/2009/08/757.jpg?resize=465%2C330&ssl=1' }, { name: 'Mona', place: "Nazereth", img: 'https://img.haarets.co.il/img/1.9037848/4234772148.jpg?width=1200&height=1200' }];
+const students: Array<Place> = [{ name: 'S', place: 'Mashad', img: 'https://i0.wp.com/www.hoshvilim.com/wp-content/uploads/2009/08/757.jpg?resize=465%2C330&ssl=1' }, { name: 'Mona', place: "Nazereth", img: 'https://img.haarets.co.il/img/1.9037848/4234772148.jpg?width=1200&height=1200' }];
 
 interface Place {
   name: string;
