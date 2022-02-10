dc5ba8ed6f055e5cef6df2f744819a208050f852
saleem <sbsaleem76@gmail.com>
2022-02-10 13:33:56 +0200

    ss

--- 04-React/01-Basic/saleem/nav-project/nav/src/App.tsx
+++ 04-React/01-Basic/saleem/nav-project/nav/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
+import './App.scss';
+
+//components
+import Card from './view/components/card/Card';
+
+
+interface Place{
+  name:string;
+  place:string;
+  img:string;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Link to={`/store/`}>
+
+        <div className='button'>login</div>
+      </Link>
+      
       </header>
     </div>
   );
