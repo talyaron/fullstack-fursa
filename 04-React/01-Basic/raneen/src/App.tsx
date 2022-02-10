029679134b584d6f23916db933d8bade99325864
raneenmandalawe <raneen.mandalawe@gmail.com>
2022-02-10 09:13:02 +0200

    page1

--- 04-React/01-Basic/raneen/src/App.tsx
+++ 04-React/01-Basic/raneen/src/App.tsx
@@ -1,18 +1,37 @@
 import './App.scss';
-
+import React from 'react';
 //components
 import Card from './view/components/card/Card';
+import Mainbar from './view/components/menu/menu';
+
+
+const clothes:Array<item> = [{name:'coat', price:500,img:'https://media.dior.com/couture/ecommerce/media/catalog/product/9/X/1586461506_841M55A3332_X1700_E01_GHC.jpg?imwidth=800'}
+,{name:'shirt',price:200, img:'https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/4c/P00533289.jpg'}];
+interface item{
+  name:string;
+  price:number;
+  img:string;
+}
+
+interface prob{
+  children: React.ReactNode;
+  open: false;
+  onClose:()=>void;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Card />
-       <Card />
-       <Card />
-       <Card />
-       <Card />
+      <Mainbar></Mainbar>
+     
+     
+      {clothes.map((item1, index)=>{
+        const {name,price,img}= item1;
+        return <Card key={index} info ={{name,price,img}} />
+      })}
       </header>
+      
     </div>
   );
 }
--- 04-React/01-Basic/raneen/src/view/components/card/Card.tsx
+++ 04-React/01-Basic/raneen/src/view/components/card/Card.tsx
@@ -1,11 +1,44 @@
-function Card() {
-    return (
-      <div className="card">
-        <h3>Card title44353454</h3>
-        <p> This is a card</p>
-      </div>
-    );
+import { useState } from "react";
+
+
+interface CardProp {
+  info: {
+    name:string;
+    price:number;
+    img:string;
+  }
+}
+
+
+
+function Card(prop: CardProp) {
+ 
+  
+  const{name, price,img} = prop.info;
+  const [color, setColor] = useState('red'); // useState(initial value);
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
   
-  export default Card;
+  function handleAddCounter() {
   
+    setColor(getRandomColor());
+    
+  }
+
+  return (
+    <div style = {{backgroundColor:color}} className="card" onClick={handleAddCounter}>
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
+      <p>Price: {price}</p>
+    </div>
+  );
+} 
+
+export default Card;
