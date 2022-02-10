1eef7690be69ab112f2c8930e83d7960e93a555a
Suzan Kassabry <suzankassabry97@gmail.com>
2022-02-10 11:45:13 +0200

    link pages

--- 04-React/01-Basic/suzan/src/index.tsx
+++ 04-React/01-Basic/suzan/src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//pages
+import Page1 from "./view/pages/page1/Page1";
+import Page2 from "./view/pages/page2/Page2";
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="page1" element={<Page1 />} />
+      <Route path="page2" element={<Page2 />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
