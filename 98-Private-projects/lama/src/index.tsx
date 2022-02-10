ccf3d07a3dbc0bceb241238a068038aa296c9efa
lama.murad.lm@gmail.com <lama.murad.lm@gmail.com>
2022-02-10 15:45:21 +0200

    react

--- 98-Private-projects/lama/src/index.tsx
+++ 98-Private-projects/lama/src/index.tsx
@@ -12,13 +12,15 @@ import {
 import App from "./App";
 import Expenses from "./view/pages/expenses/Expenses";
 import Invoices from "./view/pages/Invoices/Invoices";
+import Store from './view/pages/store/Store';
+import Product from "./view/pages/product/Product";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
@@ -33,6 +35,9 @@ render(
       <Route path="/" element={<App />} />
       <Route path="expenses" element={<Expenses />} />
       <Route path="invoices" element={<Invoices />} />
+      <Route path="store" element={<Store />} >
+        <Route path=":productId" element={<Product />} />
+      </Route>
     </Routes>
   </BrowserRouter>,
   rootElement
--- 98-Private-projects/lama/src/view/components/nav/Nav.tsx
+++ 98-Private-projects/lama/src/view/components/nav/Nav.tsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+// import './Nav.scss';
+
 function Nav() {
   return (
     <nav>
       <Link to="/expenses">Expenses</Link>
       <Link to="/invoices">Invoices</Link>
+      <Link to='/store'>Store</Link>
     </nav>
   );
 }
