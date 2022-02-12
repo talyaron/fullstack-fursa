b8c44c7f6b2ee46e1066080e6bf80bd502f32352
Suzan Kassabry <suzankassabry97@gmail.com>
2022-02-12 22:49:21 +0200

    hometask

--- 04-React/01-Basic/suzan/src/view/pages/store/Store.tsx
+++ 04-React/01-Basic/suzan/src/view/pages/store/Store.tsx
@@ -8,12 +8,17 @@ import Product from "./Product";
 const products = [{id:1,name:'cattle'}, {id:2,name:'finjan'},{id:3,name:'coffee'}];
 
 const Store = () => {
-  return <div>
+  return <div className="container">
+    <div className="container__left">
       <h1>Store</h1>
       {products.map((product:ProductProps, i)=>{
           return <Product key={i} name={product.name} id={product.id} />
       })}
+    </div>
+    <div className="container__right">
       <Outlet />
+    </div>
+      
   </div>;
 };
 
