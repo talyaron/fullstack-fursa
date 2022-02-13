cea8c3b8d3be88b49b4be6c131707f0dfe4947c0
mzmero <m.zmiro@hotmail.com>
2022-02-13 16:30:43 +0200

    cart

---
+++ 04-React/03-LiftingStateUp/zmero+ lama hometask/cart/src/views/pages/cart/cart.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+interface cartprop {
+    cartItems: any;
+    setcartItems: any;
+}
+
+
+function Cart(prop: cartprop) {
+    return (
+        <div>hi im cart</div>
+    )
+}
+export default Cart;
