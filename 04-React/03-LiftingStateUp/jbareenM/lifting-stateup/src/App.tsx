7fb7bfca7c80bfea4a52b3a80e5901474c8a07cb
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-02-14 12:59:31 +0200

    changes

--- 04-React/03-LiftingStateUp/jbareenM/lifting-stateup/src/App.tsx
+++ 04-React/03-LiftingStateUp/jbareenM/lifting-stateup/src/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Item from './view/components/Item/Item';
 import Card from './view/pages/Card/Card';
 import Store from './view/pages/Store/Store';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getAlbums();
+  }, []);
+
+  function getAlbums() {
+    return new Promise((resolve, reject) => {
+      fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(json => {
+          resolve(json);
+          setData(json);
+        }).catch(err => {
+          reject(err);
+        })
+    });
+  }
+
   return (
     <div className="App">
-      <Store />
+      {/* <Store /> */}
+      <div className="dataConatiner">
+        {data.map((album:any, index) => {
+          return (
+            <div key={index} style={{background: `url(${album.thumbnailUrl})`}} className="album">
+              <p>albumId: {album.albumId}</p>
+              <p>title: {album.title}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
