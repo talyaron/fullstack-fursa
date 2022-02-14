4420701c23a0a9160b91de71d8958eae1aafc11b
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-02-14 09:36:13 +0200

    add liftingStateUp

---
+++ 04-React/03-LiftingStateUp/jbareenM/lifting-stateup/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Item from './view/components/Item/Item';
+import Card from './view/pages/Card/Card';
+import Store from './view/pages/Store/Store';
+
+function App() {
+  return (
+    <div className="App">
+      <Store />
+    </div>
+  );
+}
+
+export default App;
