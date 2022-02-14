a6e041d0b77964deef828733bdc45935e5eacf90
Yosef <Yossefzoubi@gmail.com>
2022-02-14 09:36:09 +0200

    aa

---
+++ 04-React/03-LiftingStateUp/cart/src/dist/data.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var data = {
+  products: [{
+    id: "1",
+    name: "MacBook",
+    price: 1400,
+    image: "https://www.bug.co.il/images/site/products/2206140f-bddd-4612-9f30-9cf3b903ff92.jpg"
+  }, {
+    id: "2",
+    name: "Old Car",
+    price: 2400,
+    image: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fyc3xlbnwwfHwwfHw%3D&w=1000&q=80"
+  }, {
+    id: "3",
+    name: "W Shoes",
+    price: 1000,
+    image: "https://media.istockphoto.com/photos/white-sneaker-on-a-blue-gradient-background-mens-fashion-sport-shoe-picture-id1303978937?b=1&k=20&m=1303978937&s=170667a&w=0&h=az5Y96agxAdHt3XAv7PP9pThdiDpcQ3otWWn9YuJQRc="
+  }]
+};
+var _default = data;
+exports["default"] = _default;
