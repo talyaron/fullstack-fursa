777cfe681a3043a27e72cb7ba4253aa7617f13af
Mona-1497 <96580971+Mona-1497@users.noreply.github.com>
2022-02-15 20:20:59 +0200

    nothing

--- 04-React/04-async/OnlineTask-Weather/mona-weather/src/App.tsx
+++ 04-React/04-async/OnlineTask-Weather/mona-weather/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import {useState,useEffect} from 'react'
 import { render } from "react-dom";
@@ -18,54 +18,12 @@ import Ajv from "ajv";
 import Cities from './view/pages/cities/Cities';
 const ajv = Ajv();
 
-const weatherSchema = {
-  type: "object",
-  properties: {
-    current: { type: "object",
-    temperature:{type:"number"}},
-  },
-  required: ["current"],
-  additionalProperties: false,
-};
 
-
-const dataSchema = {
-  type: "object",
-  items: weatherSchema,
-};
-const validate = ajv.compile(dataSchema);
 
 
 
 function App() {
-  const [weather, setWeather] = useState({});
-
-  useEffect(() => {
-    
-    getWeather('New York').then((weatherDB: any) => {
-      setWeather(weatherDB);
-      console.log(weatherDB);
-     
-    }).catch(err=>{
-      console.log(err)
-    }); 
-   
-  }, []);
-  function getWeather(cityName:string) {
-    return new Promise((resolve, reject) => {
-      fetch(`http://api.weatherstack.com/current?access_key=d619498db69ef21f1475f9b54cda6417&query=${cityName}`)
-        .then((response) => response.json())
-        .then((weatherDB) => {
-          const valid = validate(weatherDB);
-          console.log(valid);
-          if (valid) resolve(weatherDB)
-          reject(validate.errors);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  }
+  
   
   return (
     <BrowserRouter>
--- 04-React/04-async/OnlineTask-Weather/mona-weather/src/view/components/card/Card.tsx
+++ 04-React/04-async/OnlineTask-Weather/mona-weather/src/view/components/card/Card.tsx
@@ -1,8 +1,55 @@
+import Ajv from "ajv";
 
+import {useState,useEffect} from 'react'
+const ajv = Ajv();
 
+const weatherSchema = {
+    type: "object",
+    properties: {
+      current: { type: "object",
+      temperature:{type:"number"}},
+    },
+    required: ["current"],
+    additionalProperties: false,
+  };
+  
+  
+  const dataSchema = {
+    type: "object",
+    items: weatherSchema,
+  };
+  const validate = ajv.compile(dataSchema);
 
 function Card()
 {
+    const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    
+    getWeather('New York').then((weatherDB: any) => {
+      setWeather(weatherDB);
+      console.log(weatherDB);
+     
+    }).catch(err=>{
+      console.log(err)
+    }); 
+   
+  }, []);
+  function getWeather(cityName:string) {
+    return new Promise((resolve, reject) => {
+      fetch(`http://api.weatherstack.com/current?access_key=d619498db69ef21f1475f9b54cda6417&query=${cityName}`)
+        .then((response) => response.json())
+        .then((weatherDB) => {
+          const valid = validate(weatherDB);
+          console.log(valid);
+          if (valid) resolve(weatherDB)
+          reject(validate.errors);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
     return(
         <div>
 
