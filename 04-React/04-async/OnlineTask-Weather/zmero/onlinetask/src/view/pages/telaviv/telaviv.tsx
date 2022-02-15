51beea9705269b966669c7bf48fb4cbf5679757c
mzmero <m.zmiro@hotmail.com>
2022-02-15 22:45:06 +0200

    complete weather app

--- 04-React/04-async/OnlineTask-Weather/zmero/onlinetask/src/view/pages/telaviv/telaviv.tsx
+++ 04-React/04-async/OnlineTask-Weather/zmero/onlinetask/src/view/pages/telaviv/telaviv.tsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 import './telaviv.scss'
+import Ajv from "ajv";
+import Menu from '../../components/menu/menu'
+const ajv = new Ajv();
+const weatherschema = {
+    type: "object",
+    properties: {
+        current: {
+            type: "object",
+            properties: {
+                temp_c: { type: 'number' }
+            }
+        },
+    },
+    required: ['current'],
+    //additionalProperties: false,
+};
 
+
+const validate = ajv.compile(weatherschema);
 
 function TelAviv() {
+    const [telaviv, setTelAviv] = useState(0);
+    let newDate = new Date()
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[newDate.getDay()];
+    let strDate = 'o Y m d'
+        .replace('Y', "" + newDate.getFullYear())
+        .replace('m', "" + newDate.getMonth() + 1)
+        .replace('d', "" + newDate.getDate())
+        .replace('o', day);
+    useEffect(() => {
+        console.log("use effect");
+
+        getData().then((e: any) => {
+            setTelAviv(e.current.temp_c);
+            console.log(e.current.temp_c);
+            console.log(telaviv)
+
+        }).catch(err => {
+            console.log(err)
+        });
+    }, []);
+    function getData() {
+        return new Promise((resolve, reject) => {
+            fetch(`http://api.weatherapi.com/v1/current.json?key=91901e4c18864d49872135614221502&q=Tel Aviv&aqi=no`)
+                .then((response) => response.json())
+                .then((e) => {
+                    const valid = validate(e);
+                    console.log(e);
+                    if (valid) resolve(e)
+                    else reject(validate.errors);
+                })
+                .catch((err) => {
+                    reject(err);
+                });
+        });
+    }
     return (
-        <div></div>
+        <div className="main">
+            <Menu></Menu>
+            <div className="background">
+                <div className="badget">
+                    <div className="date">{strDate}</div>
+                    <div className="name">Tel Aviv<sup>IL</sup></div>
+                    <div className="temp">{telaviv}<sup>°C</sup></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
