742240890c28f157dd91c4053af74c8a66d6cb35
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-02-16 12:08:58 +0200

    add nodemon

---
+++ 05-node-js/01-basic/jbareenM/dist/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/get-users', (req, res) => {
+    const users = [{ name: "a", age: 10 }, { name: "b", age: 20 }];
+    res.send(users);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
