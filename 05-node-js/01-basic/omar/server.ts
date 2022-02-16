eb2826dc2cef858769f90d3b523eb81171bb2494
omarhallomi <omarhallomi7@gmail.com>
2022-02-16 12:58:33 +0200

    a

---
+++ 05-node-js/01-basic/omar/server.ts
@@ -0,0 +1,32 @@
+ declare function require(name:string);
+const path=require("path")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//routes for data
+
+
+//static files
+const dirPath = path.join(__dirname, '../public');
+console.log(dirPath)
+app.use(express.static(dirPath));
+
+
+//data
+
+app.get('/get-all-users', (req, res) => {
+  const users = [{id:1, name:'John'}, {id:2, name:'Mary'}];
+
+  res.send(users)
+})
+
+app.get('/',(req,res)=>{
+  console.log(req);
+  res.send('Hello World! all');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
