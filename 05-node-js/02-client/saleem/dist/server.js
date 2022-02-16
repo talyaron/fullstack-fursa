66bb2619ba5d25de1857b9338c4cae083ca4938c
saleem <sbsaleem76@gmail.com>
2022-02-16 23:18:09 +0200

    aa

--- 05-node-js/02-client/saleem/dist/server.js
+++ 05-node-js/02-client/saleem/dist/server.js
@@ -8,3 +8,92 @@ app.use(express.static(dirPath));
 app.listen(port, function () {
     console.log("Example app listening on port " + port);
 });
+app.get('/get-all-users', function (req, res) {
+    var users = [
+        {
+            id: 1,
+            title: 'Fancy Product',
+            price: '40',
+            rating: 4,
+            cart: false
+        },
+        {
+            id: 2,
+            title: 'Sport Product',
+            price: '480',
+            rating: 3,
+            cart: false
+        },
+        {
+            id: 3,
+            title: 'Sale Item',
+            price: '30',
+            rating: 2,
+            cart: false
+        },
+        {
+            id: 4,
+            title: 'Popular Product',
+            price: '490',
+            rating: 5,
+            cart: false
+        },
+        {
+            id: 5,
+            title: 'Fancy Product',
+            price: '40',
+            rating: 3,
+            cart: false
+        },
+        {
+            id: 6,
+            title: 'Sport Product',
+            price: '480',
+            rating: 2,
+            cart: false
+        },
+        {
+            id: 7,
+            title: 'Sale Item',
+            price: '30',
+            rating: 4,
+            cart: false
+        },
+        {
+            id: 8,
+            title: 'Popular Product',
+            price: '490',
+            rating: 3.5,
+            cart: false
+        },
+        {
+            id: 9,
+            title: 'Fancy Product',
+            price: '40',
+            rating: 2.5,
+            cart: false
+        },
+        {
+            id: 10,
+            title: 'Sport Product',
+            price: '480',
+            rating: 1,
+            cart: false
+        },
+        {
+            id: 11,
+            title: 'Sale Item',
+            price: '30',
+            rating: 3,
+            cart: false
+        },
+        {
+            id: 12,
+            title: 'Popular Product',
+            price: '490',
+            rating: 5,
+            cart: false
+        }
+    ];
+    res.send(users);
+});
--- 05-node-js/02-client/saleem/project/src/dist/App.js
+++ 05-node-js/02-client/saleem/project/src/dist/App.js
@@ -2,79 +2,39 @@
 exports.__esModule = true;
 var react_1 = require("react");
 require("./App.css");
-var ajv_1 = require("ajv");
-var ajv = ajv_1["default"]();
+var header_component_1 = require("./components/header/header.component");
+var hero_component_1 = require("./components/hero/hero.component");
+var Item_1 = require("./components/item/Item");
 function App() {
-    var _a = react_1.useState(""), city = _a[0], setcity = _a[1];
-    var _b = react_1.useState(""), temp = _b[0], settemp = _b[1];
-    var _c = react_1.useState(""), temp_max = _c[0], settemp_max = _c[1];
-    var _d = react_1.useState(""), temp_min = _d[0], settemp_min = _d[1];
-    var _e = react_1.useState(""), search = _e[0], setSearch = _e[1];
-    var CitySchema = {
-        type: "array",
-        properties: {
-            temp: { type: "number" },
-            temp_min: { type: "number" },
-            temp_max: { type: "number" }
-        },
-        required: ["temp", "temp_min", "temp_max"],
-        additionalProperties: false
-    };
-    var dataSchema = {
-        type: "object",
-        items: CitySchema
-    };
-    var validate = ajv.compile(dataSchema);
-    function findCity() {
-        return new Promise(function (resolve, reject) {
-            fetch("http://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=metric&appid=4ebb9418ca605fa1931880e565ec065c")
-                .then(function (response) { return response.json(); })
-                .then(function (dataDB) {
-                var valid = validate(dataDB);
-                console.log(valid);
-                if (valid)
-                    resolve(dataDB);
-                else
-                    reject(validate.errors);
-            })["catch"](function (err) {
-                reject(err);
-            });
+    var _a = react_1.useState([]), data = _a[0], setData = _a[1];
+    var _b = react_1.useState(0), cartCount = _b[0], setcartCount = _b[1];
+    react_1.useEffect(function () {
+        fetch('/get-all-users').then(function (res) { return res.json(); }).then(function (data) {
+            console.log(data);
+            setData(data);
         });
+    }, []);
+    function addToCart(event) {
+        console.log(event.target.dataset.id);
+        var index = parseInt(event.target.dataset.id);
+        var tempData = data;
+        tempData[index].cart = true;
+        setcartCount(cartCount + 1);
+        setData(tempData);
     }
-    function setData() {
-        findCity().then(function (whetherDB) {
-            settemp(whetherDB.main.temp);
-            settemp_max(whetherDB.main.temp_max);
-            settemp_min(whetherDB.main.temp_min);
-            setcity(search);
-            console.log(temp_max);
-            console.log(whetherDB);
-        })["catch"](function (err) {
-            console.log(err);
-        });
+    function removeFromCart(event) {
+        var index = parseInt(event.target.dataset.id);
+        var tempData = data;
+        tempData[index].cart = false;
+        if (cartCount > 0) {
+            setcartCount(cartCount - 1);
+        }
+        setData(tempData);
     }
-    return (react_1["default"].createElement("div", { className: "box" },
-        react_1["default"].createElement("div", { className: "InputData" },
-            react_1["default"].createElement("h2", null, "insert your city"),
-            react_1["default"].createElement("input", { value: search, type: "search", className: "InputField", onChange: function (event) {
-                    setSearch(event.target.value);
-                } }),
-            react_1["default"].createElement("input", { value: "Submit", type: "button", className: "btn", onClick: setData }),
-            !city ?
-                (react_1["default"].createElement("div", null,
-                    react_1["default"].createElement("h3", null,
-                        react_1["default"].createElement("b", null, "No Data Found")))) :
-                (react_1["default"].createElement("div", null,
-                    react_1["default"].createElement("h1", { className: "temp" },
-                        "curernt temp  ",
-                        temp),
-                    react_1["default"].createElement("h3", { className: "tempmin_max" },
-                        "min temp:",
-                        temp_min,
-                        " Cel"),
-                    react_1["default"].createElement("h3", { className: "tempmin_max" },
-                        "max temp:",
-                        temp_max,
-                        "Cel"))))));
+    return (react_1["default"].createElement("div", { className: "App" },
+        react_1["default"].createElement(header_component_1["default"], { count: cartCount }),
+        react_1["default"].createElement(hero_component_1["default"], null),
+        react_1["default"].createElement("div", { className: "container" },
+            react_1["default"].createElement("div", { className: "row" }, data.map(function (item, index) { return (react_1["default"].createElement(Item_1["default"], { index: index, add: addToCart, remove: removeFromCart, id: item.id, title: item.title, price: item.price, incart: item.cart })); })))));
 }
 exports["default"] = App;
