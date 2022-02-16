1b3fb724064d2a5622a57892ae2743d941dc14e8
lama.murad.lm@gmail.com <lama.murad.lm@gmail.com>
2022-02-16 11:32:50 +0200

    ..

--- 05-node-js/01-basic/Lama/server.ts
+++ 05-node-js/01-basic/Lama/server.ts
@@ -4,3 +4,37 @@ const x = 3;
 console.log(x*4);
 console.log(process.env.USER);
 
+// declare function require(name:string);
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+
+// //routes for data
+
+
+// //static files
+
+// app.use(express.static('public'));
+
+
+// //data
+
+// app.get('/getadat/all-users', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get('/all',(req,res)=>{
+//   console.log(req);
+//   res.send('Hello World! all');
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+
