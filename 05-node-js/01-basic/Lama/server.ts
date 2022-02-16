8a242fd7bfdd53f486b4ecfe2a0240f2cbdbf786
lama.murad.lm@gmail.com <lama.murad.lm@gmail.com>
2022-02-16 12:35:23 +0200

    ..

--- 05-node-js/01-basic/Lama/server.ts
+++ 05-node-js/01-basic/Lama/server.ts
@@ -1,38 +1,43 @@
-console.log('Hi all 2tre');
+// console.log('Hi all 2tre');
 
-const x = 3;
-console.log(x*4);
-console.log(process.env.USER);
+// const x = 3;
+// console.log(x*4);
+// console.log(process.env.USER);
 
 // declare function require(name:string);
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-
-// //routes for data
+const path=require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
-// //static files
-
-// app.use(express.static('public'));
+//routes for data
 
 
-// //data
+//static files
 
-// app.get('/getadat/all-users', (req, res) => {
-//   res.send('Hello World!')
-// })
+// const dirPath = path.join(__dirname, '../public');
+// console.log(dirPath)
 
-// app.get('/all',(req,res)=>{
-//   console.log(req);
-//   res.send('Hello World! all');
-// })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+// app.use(express.static('dirPath'));
+app.use(express.static('public'));
+
+
+//data
+
+app.get('/getadat/all-users', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/all',(req,res)=>{
+  console.log(req);
+  res.send('Hello World! all');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
