d834d5f1212ec3e30333bed15b95044f4a2d975b
saleem <sbsaleem76@gmail.com>
2022-02-16 09:40:16 +0200

    a

--- 04-React/03-LiftingStateUp/saleem/ex8/src/components/hero/hero.component.jsx
+++ 04-React/03-LiftingStateUp/saleem/ex8/src/components/hero/hero.component.jsx
@@ -9,7 +9,6 @@ const HeroMessage = () => {
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
                 </div>
             </div>
         </header>
