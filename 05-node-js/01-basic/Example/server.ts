0b8fcf41876effdcc422518c4e982a8c803ac799
Tal <tal.yaron@gmail.com>
2022-02-16 11:08:22 +0200

    node

--- 05-node-js/01-basic/Example/server.ts
+++ 05-node-js/01-basic/Example/server.ts
@@ -1,6 +1,26 @@
-console.log('Hi all 2tre');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const x = 3;
-console.log(x*4);
-console.log(process.env.USER);
 
+//routes for data
+
+
+//static files
+
+app.use(express.static('public'));
+
+//data
+
+app.get('/getadat/all-users', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/all',(req,res)=>{
+  console.log(req);
+  res.send('Hello World! all');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
