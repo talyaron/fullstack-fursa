8eb122d18e90a16b6bd848449b7de3987242f347
mzmero <m.zmiro@hotmail.com>
2022-02-16 18:46:31 +0200

    homeTask

--- 98-Private-projects/zmero/client/src/view/pages/explore/explore.tsx
+++ 98-Private-projects/zmero/client/src/view/pages/explore/explore.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from '../../components/restaurantCard/restaurantCard';
 import Search from '../../components/search/search'
 import Navbar from '../../components/menu/menu';
@@ -12,11 +12,21 @@ interface cardProp {
     booking: number;
 }
 
-const Data: Array<cardProp> = [{ id: 1, name: 'shila-sharon cohen Kitchen & bar', image: 'https://media-cdn.tripadvisor.com/media/photo-s/03/7a/47/6d/shila.jpg', booking: 27 },
-{ id: 2, name: 'Cafe Popular', image: 'https://cdn.archilovers.com/projects/c_383_f83bb625-ff28-414b-a700-8e4bebde941b.jpg', booking: 22 },
-{ id: 3, name: 'Taizu', image: 'https://bestrest.rest/wp-content/uploads/2019/05/TAIZU_017.jpg', booking: 20 },
-{ id: 5, name: 'Hakosem', image: 'https://cdn.istores.co.il/image/upload/if_ar_gt_2:1/c_fill,h_662,w_555/c_fill,h_662,w_555/if_else/c_fill,q_100,w_555/if_end/dpr_2/v1614106887/clients/109701/5e9b9717137eea988b88592d0861cc3a64d59fcd.jpg', booking: 15 }, { id: 4, name: 'La Regence', image: 'https://images.jpost.com/image/upload/f_auto,fl_lossy/t_JM_ArticleMainImageFaceDetect/399693', booking: 5 }]
-function explore() {
+
+
+function Explore() {
+    const [famousRestaurants, setFamousRestaurant] = useState([{ id: 0, name: "", image: "", booking: 0 }]);
+    useEffect(() => {
+        fetch('/get-famous-restaurants')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setFamousRestaurant(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, []);
     return (
         <div>
             <Navbar></Navbar>
@@ -35,7 +45,7 @@ function explore() {
                         <div className="exploremain__popular__view">View All</div>
                     </header>
                     <div className="exploremain__popular__grid">
-                        {Data.map((rest, index) => {
+                        {famousRestaurants.map((rest, index) => {
                             return <Card key={index} id={rest.id} name={rest.name} image={rest.image} booking={rest.booking}></Card>
                         })}
                     </div>
@@ -51,4 +61,4 @@ function explore() {
     );
 }
 
-export default explore
+export default Explore
--- 98-Private-projects/zmero/server.ts
+++ 98-Private-projects/zmero/server.ts
@@ -4,7 +4,20 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.use(express.static('client/public'));
+app.use(express.static('client/build'));
+
+//gets
+
+app.get('/get-famous-restaurants', (req, res) => {
+    const Data = [{ id: 1, name: 'shila-sharon cohen Kitchen & bar', image: 'https://media-cdn.tripadvisor.com/media/photo-s/03/7a/47/6d/shila.jpg', booking: 27 },
+    { id: 2, name: 'Cafe Popular', image: 'https://cdn.archilovers.com/projects/c_383_f83bb625-ff28-414b-a700-8e4bebde941b.jpg', booking: 22 },
+    { id: 3, name: 'Taizu', image: 'https://bestrest.rest/wp-content/uploads/2019/05/TAIZU_017.jpg', booking: 20 },
+    { id: 5, name: 'Hakosem', image: 'https://cdn.istores.co.il/image/upload/if_ar_gt_2:1/c_fill,h_662,w_555/c_fill,h_662,w_555/if_else/c_fill,q_100,w_555/if_end/dpr_2/v1614106887/clients/109701/5e9b9717137eea988b88592d0861cc3a64d59fcd.jpg', booking: 15 }, { id: 4, name: 'La Regence', image: 'https://images.jpost.com/image/upload/f_auto,fl_lossy/t_JM_ArticleMainImageFaceDetect/399693', booking: 5 }]
+    console.log(Data)
+
+    res.send(Data)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
