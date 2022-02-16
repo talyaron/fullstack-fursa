bff3bcd9b0f0957b84a28daa9513ac86f8db235d
DimaAbbas <dimaabbas25@gmail.com>
2022-02-16 11:56:20 +0200

    ...

--- 05-node-js/01-basic/dima/dist/server.js
+++ 05-node-js/01-basic/dima/dist/server.js
@@ -1,1 +1,16 @@
-console.log('Hi all 2tre');
+var express = require('express');
+var app = express();
+var port = 3000;
+//static files
+app.use(express.static('public'));
+//data
+app.get('/getadat/all-users', function (req, res) {
+    res.send('Hello World!');
+});
+app.get('/all', function (req, res) {
+    console.log(req);
+    res.send('Hello World! all');
+});
+app.listen(port, function () {
+    console.log("Example app listening on port " + port);
+});
--- 05-node-js/01-basic/dima/server.ts
+++ 05-node-js/01-basic/dima/server.ts
@@ -1,1 +1,25 @@
-console.log('Hi all 2tre');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//static files
+
+app.use(express.static('public'));
+
+
+//data
+
+app.get('/getadat/all-users', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/all',(req,res)=>{
+  console.log(req);
+  res.send('Hello World! all');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
