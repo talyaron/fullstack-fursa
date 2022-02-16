d078bbfa11526ff46e268f0d2c5468d7d898ba88
DimaAbbas <dimaabbas25@gmail.com>
2022-02-16 19:54:15 +0200

    hometask

---
+++ 98-Private-projects/Dima_/client/src/view/recipes/Recipes.tsx
@@ -0,0 +1,43 @@
+import addIcon from './add.png';
+import img1 from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
+import img4 from './4.jpg';
+import img5 from './5.jpg';
+import { useState } from 'react';
+
+function Recipes() {
+
+    return (
+        <div className="recipes">
+            <h2 className='title'>Recipes</h2>
+            <div className="row1">
+                <div className="item">
+                    <div className='itemImg'><img src={img1} alt="" /></div>
+                    <div className='title'><p>recipe 1</p></div>
+                </div>
+                <div className="item">
+                    <div className='itemImg'><img src={img2} alt="" /></div>
+                    <div className='title'><p>recipe 2</p></div>
+                </div>
+                <div className="item">
+                    <div className='itemImg'><img src={img3} alt="" /></div>
+                    <div className='title'><p>recipe 3</p></div>
+                </div>
+            </div>
+            <div className="row2">
+                <div className="item">
+                    <div className='itemImg'><img src={img4} alt="" /></div>
+                    <div className='title'><p>recipe 4</p></div>
+                </div>
+                <div className="item">
+                    <div className='itemImg'><img src={img5} alt="" /></div>
+                    <div className='title'><p>recipe 5</p></div>
+                </div>
+            </div>
+            <div className="add"><img src={addIcon} alt="" /></div>
+        </div>
+    );
+}
+
+export default Recipes;
---
+++ 98-Private-projects/Dima_/dist/server.js
@@ -0,0 +1,8 @@
+//declare function require(name: string);
+const express = require('express');
+const app = express();
+const port = 4000;
+app.use(express.static('client/build'));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
