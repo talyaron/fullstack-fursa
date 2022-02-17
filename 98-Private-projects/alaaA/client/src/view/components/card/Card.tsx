cd88bb214fb4e23f61a15c1daaa939b89783c941
AlaaAga <alaa.ali.aga1@gmail.com>
2022-02-17 11:00:50 +0200

    onlineMarkit

--- 98-Private-projects/alaaA/client/src/view/components/card/Card.tsx
+++ 98-Private-projects/alaaA/client/src/view/components/card/Card.tsx
@@ -42,15 +42,7 @@ function Card(prop: product) {
 
   const {id, name, price,quantity, description,  Url,productsCart,setproductsCart } = prop;
   const [color, setColor] = useState('red');
-  function generateRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var newColor = '#';
-    for (var i = 0; i < 6; i++) {
-        newColor += letters[Math.floor(Math.random() * 16)];
-    }
-    setColor(newColor);
-
-}
+  
 const [mytext, setmytext] = useState('hi , alaa wants to write me');
     function moveInput(e: any) {
         try {
@@ -63,7 +55,7 @@ const [mytext, setmytext] = useState('hi , alaa wants to write me');
 
   return (
 
-      <div className="card"  style={{ backgroundColor: color }} onClick={generateRandomColor} >
+      <div className="card"  style={{ backgroundColor: color }} >
      
         <img src={Url} alt="" />
         <h3>{name}</h3>
