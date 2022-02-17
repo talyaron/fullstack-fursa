e60cd697195307734f0943ff50f3b8717c65abaa
Suzan Kassabry <suzankassabry97@gmail.com>
2022-02-17 10:26:15 +0200

    xxxx

--- 98-Private-projects/Suzan2/client/src/view/pages/students/Students.tsx
+++ 98-Private-projects/Suzan2/client/src/view/pages/students/Students.tsx
@@ -2,14 +2,25 @@ import { Link, Outlet } from "react-router-dom";
 import MainHeader from "../../components/main header/MainHeader";
 import SearchBar from "../../components/searchBar/SearchBar";
 import Student, {studentInfo} from './Student'; 
+import {useState, useEffect} from 'react';
 
 function Students() {
 
-    const arr:Array<studentInfo> = [
-        {info: {firstName:'Suzan', lastName:'Kassabry', father:'Emad', mother:'Linda', id:'111111111', phone:'0537756048', fphone:'0537756048', mphone:'0537756048', email:'suzankassabry97@gmail.com'}},
-        {info: {firstName:'Suzan', lastName:'Kassabry', father:'Emad', mother:'Linda', id:'222222222', phone:'0537756048', fphone:'0537756048', mphone:'0537756048', email:'suzankassabry97@gmail.com'}},
-        {info: {firstName:'Suzan', lastName:'Kassabry', father:'Emad', mother:'Linda', id:'333333333', phone:'0537756048', fphone:'0537756048', mphone:'0537756048', email:'suzankassabry97@gmail.com'}}
-    ]
+    // const students:Array<studentInfo> = [
+    //     {info: {firstName:'Maria', lastName:'Kassabry', father:'Emad', mother:'Linda', id:'111111111', phone:'0537756048', fphone:'0537756048', mphone:'0537756048', email:'suzankassabry97@gmail.com'}},
+    //     {info: {firstName:'Suzan', lastName:'Kassabry', father:'Emad', mother:'Linda', id:'222222222', phone:'0537756048', fphone:'0537756048', mphone:'0537756048', email:'suzankassabry97@gmail.com'}},
+    //     {info: {firstName:'Suzan', lastName:'Kassabry', father:'Emad', mother:'Linda', id:'333333333', phone:'0537756048', fphone:'0537756048', mphone:'0537756048', email:'suzankassabry97@gmail.com'}}
+    // ]
+
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        console.log('useEffect');
+        fetch('/get-all-students').then(res => res.json()).then(data => {
+            setStudents(data);
+            console.log(data)
+        })
+    }, []);
 
     return (
         <div>
@@ -19,9 +30,9 @@ function Students() {
             <SearchBar></SearchBar>
             <div className="wrapper">
                 <div className="left">
-                    {arr.map((student, index)=>{
+                    {students.map((student, index)=>{
                         //const {info: {firstName, lastName, father, mother, id, phone, fphone, mphone, email}} = student;
-                        return <Student key={index} info={student.info} />
+                        return <Student key={index} info={student} />
                     })}
                 </div>
                 <div className="right">
