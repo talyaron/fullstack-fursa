7afbbb55ef29f82ec073649361ffe5aadc67169d
Suzan Kassabry <suzankassabry97@gmail.com>
2022-02-21 00:42:36 +0200

    project continued ....

--- 98-Private-projects/suzan/src/view/school/pages/classInfo/ClassInfo.tsx
+++ 98-Private-projects/suzan/src/view/school/pages/classInfo/ClassInfo.tsx
@@ -1,14 +1,19 @@
+import Typography from '@mui/material/Typography';
 import react from 'react-router-dom'
 import ClassTabsBar from '../../components/classTabsBar/ClassTabsBar';
 import SchoolResponsiveAppBar from '../../components/header/AppBar';
 import './ClassInfo.scss';
 
-export default function ClassInfo(){
-    return(
+export default function ClassInfo() {
+    return (
         <div className="container">
 
             <div className="bar">
                 <SchoolResponsiveAppBar></SchoolResponsiveAppBar>
+            </div>
+
+            <div className="className">
+                <Typography variant='h3' align='center'>Class Name</Typography>
             </div>
 
             <div className="tabsBar">
