b2be4171ad59e5c2f6003da386d0e1d0666256c2
AlaaAga <alaa.ali.aga1@gmail.com>
2022-02-21 09:54:19 +0200

    createNewClient

--- 98-Private-projects/alaaA/client/src/App.tsx
+++ 98-Private-projects/alaaA/client/src/App.tsx
@@ -1,69 +1,26 @@
-import React, { useState } from 'react';
-import { render } from "react-dom";
-import './index.scss';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import HomePage from "./view/pages/homePage/homePage";
-import Login from "./view/pages/logIn/logIn";
-import SignUp from "./view/pages/signUp/signUp";
-import AboutUs from "./view/pages/AboutUs/aboutUs";
-import Group from "./view/pages/Groups/group";
-import CreateGroup from "./view/pages/CreateGroup/createGroup";
-import Store from "./view/pages/store/store";
-import Mygroups from './view/pages/mygroups/mygroups';
-import Cart from './view/components/Cart/Cart'
-import AboutUsIn from "./view/pages/aboutUsIn/aboutUsIn"
-import Help from "./view/pages/help/help"
-import Settings from "./view/pages/settings/settings"
-
-
-interface product {
-  id:number;
-  name: string;
-  price: number;
-  quantity: number;
-  description: string;
-  Url: string;
-  productsCart:any;
-  setproductsCart:any;
-}
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-const [productsCart, setproductsCart] = useState([]);
-
-
-
-
-return(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<HomePage/>} />
-    <Route path="Login" element={<Login />}></Route>
-    <Route path="Cart" element={<Cart />} />
-    <Route path="Login" element={<HomePage />} />
-    <Route path="signUp" element={<SignUp />} />
-    <Route path="AboutUs" element={<AboutUs />} />
-    <Route path="Group" element={<Group />} />
-    <Route path="CreateGroup" element={<CreateGroup />} />
-    <Route path="HomePage" element={<HomePage />} />
-    <Route path="Store" element={<Store />} />
-    <Route path="mygroups" element={<Mygroups />} />
-    <Route path="aboutUsIn" element={<AboutUsIn />} />
-    <Route path="help" element={<Help />} />
-    <Route path="settings" element={<Settings />} />
-
-
-
-    {/* <Route></Route> */}
-    {/* </Route> */}
-    
-
-    {/* <Route path="invoices" element={<Invoices />} /> */}
-  </Routes>
-</BrowserRouter>)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- 98-Private-projects/alaaA/client/src/index.tsx
+++ 98-Private-projects/alaaA/client/src/index.tsx
@@ -1,20 +1,17 @@
-
-import React from 'react'
-import ReactDOM from "react-dom";
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import App from "./App";
-import { useState } from 'react';
-
-const rootElement = document.getElementById("root");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
-    </React.StrictMode>,
-  rootElement
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
