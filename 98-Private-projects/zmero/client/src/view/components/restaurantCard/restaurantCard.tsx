026aa88d207ac8ef74f3c6384f115d1a7de6613a
mzmero <m.zmiro@hotmail.com>
2022-02-22 14:16:16 +0200

    photos effect

--- 98-Private-projects/zmero/client/src/view/components/restaurantCard/restaurantCard.tsx
+++ 98-Private-projects/zmero/client/src/view/components/restaurantCard/restaurantCard.tsx
@@ -3,15 +3,7 @@ import Logo from './raise.svg'
 import './restaurantCard.scss'
 import { Link } from 'react-router-dom';
 import Rating from '@mui/material/Rating'
-import Modal from '@mui/material/Modal'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
-import TimePicker from '@mui/lab/TimePicker';
-import Button from '@mui/material/Button';
+import ReserveModal from '../../components/reserveModal/reserveModal'
 interface cardProp {
     id: number;
     name: string;
@@ -22,7 +14,7 @@ interface cardProp {
 }
 
 
-function RestaurantCard(prop: cardProp) {
+function RestaurantCard(props: cardProp) {
     const [openModal, setOpenModal] = useState(false);
     const [reserveDate, setReserveDate] = React.useState<Date | null>(new Date());
     const [reserveTime, setReserveTime] = useState<number | null>(0)
@@ -40,67 +32,27 @@ function RestaurantCard(prop: cardProp) {
     function openReserve(e: any) {
         e.preventDefault();
         setOpenModal(true);
-
     }
     return (
-        <Link to={`/Restaurant/${prop.id}`}>
+        <Link to={`/Restaurant/${props.id}`}>
             <div className="restaurantCard">
-                <img src={prop.image}></img>
+                <img src={props.image}></img>
                 <div className="restaurantCard__title">
-                    <h3>{prop.name}</h3>
+                    <h3>{props.name}</h3>
                 </div>
                 <div>
-                    <Rating style={{ color: '#931b23' }} name="read-only" value={prop.stars} readOnly />
+                    <Rating style={{ color: '#931b23' }} name="read-only" value={props.stars} readOnly />
                 </div>
                 <div className="restaurantCard__booking">
                     <img src={Logo} ></img>
                     <div className="restaurantCard__booking__title">
-                        Booked {prop.booking} times today!
+                        Booked {props.booking} times today!
                 </div>
                 </div>
                 <div className="restaurantCard__reserve">
                     <button className="restaurantCard__reserve__btn" onClick={openReserve}>Find next availabe</button>
                 </div>
-                <Modal
-                    open={openModal}
-                    onClose={() => setOpenModal(false)}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                    onClick={(e: any) => (e.preventDefault())}
-                >
-                    <Box sx={style} >
-                        <Typography variant="h6" component="h2">
-                            Pick Up a Date
-                        </Typography>
-                        <Typography sx={{ mt: 2 }}>
-                            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                <DatePicker
-                                    label="Date"
-                                    value={reserveDate}
-                                    onChange={(newValue) => {
-                                        setReserveDate(newValue);
-                                    }}
-                                    renderInput={(params) => <TextField {...params} />}
-                                />
-                            </LocalizationProvider>
-                        </Typography>
-                        <Typography sx={{ mt: 2 }}>
-                            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                <TimePicker
-                                    label="Time"
-                                    value={reserveTime}
-                                    onChange={(newValue) => {
-                                        setReserveTime(newValue);
-                                    }}
-                                    renderInput={(params) => <TextField {...params} />}
-                                />
-                            </LocalizationProvider>
-                        </Typography>
-                        <Typography align="right" sx={{ mt: 3 }}>
-                            <Button style={{ backgroundColor: '#2a945b' }} variant="contained">Reserve Now</Button>
-                        </Typography>
-                    </Box>
-                </Modal>
+                <ReserveModal restaurantID={props.id} openModal={openModal} setOpenModal={setOpenModal} />
             </div>
         </Link >
     )
--- 98-Private-projects/zmero/client/src/view/pages/restaurant/restaurant.tsx
+++ 98-Private-projects/zmero/client/src/view/pages/restaurant/restaurant.tsx
@@ -7,6 +7,16 @@ import Button from '@mui/material/Button'
 import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
+import ReserveModal from '../../components/reserveModal/reserveModal'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
 
 interface cardProp {
     id: number;
@@ -19,9 +29,32 @@ interface cardProp {
     photos: Array<string>;
 }
 
+
 function Restaurant() {
+    const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = 2;
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step: number) => {
+        setActiveStep(step);
+    };
+
     const { RestaurantId } = useParams();
+    const [openModal, setOpenModal] = useState(false);
+    const [openPhoto, setOpenPhoto] = useState(false);
     const [Restaurant, setRestaurant] = useState<cardProp>({ id: 0, name: "", image: "", booking: 0, region: "", stars: 0, category: "", photos: [] })
+    function openReserve(e: any) {
+        e.preventDefault();
+        setOpenModal(true);
+    }
     useEffect(() => {
         axios.get(`http://localhost:3004/Restaurants/${RestaurantId}`).then(({ data }) => {
             setRestaurant(data)
@@ -29,13 +62,16 @@ function Restaurant() {
         );
     }, []);
 
-    //const imageArr = [{ url: 'https://www.w3schools.com/howto/img_nature_wide.jpg' }, { url: 'https://www.w3schools.com/howto/img_snow_wide.jpg' }]
+    const images = [{ url: 'https://www.w3schools.com/howto/img_nature_wide.jpg' }, { url: 'https://www.w3schools.com/howto/img_snow_wide.jpg' }]
+    function photoSlider() {
+        setOpenPhoto(true);
+    }
 
     return (
         <div>
             <Menu></Menu>
             <div className="rest">
-                <div className="rest__images">
+                <div className="rest__images" onClick={photoSlider}>
                     <div className="rest__images__photo" style={{ backgroundImage: `url(${Restaurant.photos[0]})` }}></div>
                     <div className="rest__images__photo" style={{ backgroundImage: `url(${Restaurant.photos[1]})` }}></div>
                 </div>
@@ -56,11 +92,76 @@ function Restaurant() {
                             </div>
                         </div>
                         <div className="rest__main__header__right">
-                            <Button style={{ backgroundColor: '#2a945b' }} fullWidth variant="contained">Reserve Now</Button>
+                            <Button style={{ backgroundColor: '#2a945b' }} fullWidth variant="contained" onClick={openReserve}>Reserve Now</Button>
                         </div>
                     </div>
                 </div>
+
             </div >
+            <ReserveModal restaurantID={Number(RestaurantId)} openModal={openModal} setOpenModal={setOpenModal} />
+            <Modal
+                open={openPhoto}
+                onClose={() => setOpenPhoto(false)}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+                    <AutoPlaySwipeableViews
+                        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                        index={activeStep}
+                        onChangeIndex={handleStepChange}
+                        enableMouseEvents
+                    >
+                        {images.map((step, index) => (
+                            <div key={index}>
+                                {Math.abs(activeStep - index) <= 2 ? (
+                                    <Box
+                                        component="img"
+                                        sx={{
+                                            height: 255,
+                                            display: 'block',
+                                            maxWidth: 400,
+                                            overflow: 'hidden',
+                                            width: '100%',
+                                        }}
+                                        src={step.url}
+
+                                    />
+                                ) : null}
+                            </div>
+                        ))}
+                    </AutoPlaySwipeableViews>
+                    <MobileStepper
+                        steps={maxSteps}
+                        position="static"
+                        activeStep={activeStep}
+                        nextButton={
+                            <Button
+                                size="small"
+                                onClick={handleNext}
+                                disabled={activeStep === maxSteps - 1}
+                            >
+                                Next
+            {theme.direction === 'rtl' ? (
+                                    <KeyboardArrowLeft />
+                                ) : (
+                                        <KeyboardArrowRight />
+                                    )}
+                            </Button>
+                        }
+                        backButton={
+                            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                                {theme.direction === 'rtl' ? (
+                                    <KeyboardArrowRight />
+                                ) : (
+                                        <KeyboardArrowLeft />
+                                    )}
+            Back
+          </Button>
+                        }
+                    />
+                </Box>
+            </Modal>
         </div>
     )
 }
