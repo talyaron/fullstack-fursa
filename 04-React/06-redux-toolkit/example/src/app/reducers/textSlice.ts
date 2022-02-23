29b103c3f1534dfb20cbe287498046ba8019ae4d
Tal Yaron <tal.yaron@gmail.com>
2022-02-23 14:10:43 +0200

    cont

--- 04-React/06-redux-toolkit/example/src/app/reducers/textSlice.ts
+++ 04-React/06-redux-toolkit/example/src/app/reducers/textSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../../app/store';
+import { RootState, AppThunk } from '../store';
 import {getJoke} from './textApi';
 
 export interface TextProps {
