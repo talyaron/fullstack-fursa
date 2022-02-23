580dcc2fdcaecee36f47260f0dd9a1c0fbe4eb7f
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-02-23 12:02:44 +0200

    some changes

--- 04-React/06-redux-toolkit/onlineTasks/jbareenM/ibring/src/components/page/Page.tsx
+++ 04-React/06-redux-toolkit/onlineTasks/jbareenM/ibring/src/components/page/Page.tsx
@@ -5,22 +5,25 @@ import { select } from '../../features/pageSelector/pageReducer';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 
 function Page(props: any) {
-    const { url } = props;
+    const { content, url } = props;
     const dispatch = useAppDispatch();
     const [urlSelected, setUrlSelected] = useState('');
 
     function handleChoosePage(ev: any) {
         ev.preventDefault();
-        dispatch(select(url));
+        dispatch(select({url:url, title: content.title, data: content.data}));
     }
 
     return (
         <div className="pageContainer" onClick={handleChoosePage}>
             <div className="pageHeader">
+                {url !== ""? 
                 <img src={url} alt="" />
+                : <label>there is no selected image</label>}
             </div>
             <div className="pageContent">
-                content
+                <div className="pageTitle">{content.title}</div>
+                <div className="pageData">{content.data}</div>
             </div>
         </div>
     )
