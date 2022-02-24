a192d6bb9e6d7281ce235eee1290eaa1034590b7
Majdna <48679257+Majdna@users.noreply.github.com>
2022-02-24 13:56:58 +0200

    2

--- 98-Private-projects/alaaA/client/src/view/fitures/myGroups/myGroupsSlice.ts
+++ 98-Private-projects/alaaA/client/src/view/fitures/myGroups/myGroupsSlice.ts
@@ -1,5 +1,8 @@
 
- 
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { RootState } from '../../../app/store';
+
  interface mygroup{
      id: string
      groupName: string
@@ -12,7 +15,34 @@
 const initialState:mygroups={
     arr : []
 }
+export const getMygroupsAsync = createAsyncThunk(
+    'mygroups/fetchMyGroups',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get('http://localhost:3004/group/')
+            const data = response.data
+            return data
+        } catch (error:any) {
+            thunkAPI.rejectWithValue(error.response.data)
+        }
+
+    }
+);
 
 export const mygroupsReducer= createSlice({
-    
+    name: 'mygroups', 
+    initialState,
+    reducers:{
+
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(getMygroupsAsync.fulfilled, (state, action)=>{
+            state.arr= action.payload;
+        })
+    }
 })
+
+export const selectMygroups = (state: RootState)=> state.mygroups
+
+
+export default mygroupsReducer.reducer;
