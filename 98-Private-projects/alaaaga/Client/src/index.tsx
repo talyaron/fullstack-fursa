c1976ac5076f4ce9fa563eb527ca5c8ad02a5836
AlaaAga <alaa.ali.aga1@gmail.com>
2022-02-25 17:11:43 +0200

    Auto stash before revert of "redux comment"

--- 98-Private-projects/alaaaga/Client/src/index.tsx
+++ 98-Private-projects/alaaaga/Client/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+<<<<<<< Updated upstream:98-Private-projects/alaaA/client/src/index.tsx
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+=======
+//import { store } from './app/store';
+// import { Provider } from 'react-redux';
+>>>>>>> Stashed changes:98-Private-projects/alaaaga/Client/src/index.tsx
 import * as serviceWorker from './serviceWorker';
 
 import { render } from "react-dom";
