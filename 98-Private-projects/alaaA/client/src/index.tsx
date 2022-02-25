0fc9339110a1c0e6d4dfdaf4c5fc2e35d0a3713a
AlaaAga <alaa.ali.aga1@gmail.com>
2022-02-25 16:59:45 +0200

    000

--- 98-Private-projects/alaaA/client/src/index.tsx
+++ 98-Private-projects/alaaA/client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 //import { store } from './app/store';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 import { render } from "react-dom";
