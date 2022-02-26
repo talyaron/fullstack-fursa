92ae77118f03a6113f5136f7c3ed4c716e862838
Suzan Kassabry <suzankassabry97@gmail.com>
2022-02-26 19:25:37 +0200

    ....

--- 98-Private-projects/suzan/src/view/teacher/pages/course/Course.tsx
+++ 98-Private-projects/suzan/src/view/teacher/pages/course/Course.tsx
@@ -4,11 +4,16 @@ import { selectedClassName, selectedCourseName } from "../../../../app/reducers/
 import CourseResponsiveAppBar from "../../components/courseHeader/CourseAppBar";
 import MaterialSection from "../../components/materialSection/MaterialSection";
 import UpdatesList from "../../components/updatesList/UpdatesList";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAppSelector, useAppDispatch } from '../../../../app/hooks';
 import './Course.scss';
 import { courseMaterial, getMaterialAsync } from "../../../../app/reducers/teacher/CourseDataSlice";
+import Button from "@mui/material/Button";
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import NewMaterialDialog from "../../components/newMaterialDialog/NewMaterialDialog";
+import NewHomeworkDialog from "../../components/newHomeworkDialog/NewHomeworkDialog";
+import NewExamDialog from "../../components/newExamDialog/NewExamDialog";
 
 // const materials = [
 //     { title: 'material title1', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
@@ -29,12 +34,27 @@ const updates = [
 export default function Course() {
     const courseName = useAppSelector(selectedCourseName);
     const className = useAppSelector(selectedClassName);
-    
+    const [openMaterialDialog, setOpenMaterialDialog] = useState(false);
+    const [openHomeworkDialog, setOpenHomeworkDialog] = useState(false);
+    const [openExamDialog, setOpenExamDialog] = useState(false);
+
     const dispatch = useAppDispatch();
     useEffect(() => {
         dispatch(getMaterialAsync());
     }, []);
     const materials = useAppSelector(courseMaterial);
+
+    function handleOpenMaterialDialog(){
+        setOpenMaterialDialog(true)
+    }
+
+    function handleOpenHomeworkDialog(){
+        setOpenHomeworkDialog(true)
+    }
+
+    function handleOpenExamDialog() {
+        setOpenExamDialog(true)
+    }
 
     return (
         <div>
@@ -44,11 +64,20 @@ export default function Course() {
 
             <div className="subContainer">
 
-
-
                 <div className="title">
                     <Typography variant='h3' align='center'>{courseName}</Typography>
                     <Typography variant='h5' align='center'>{className}</Typography>
+                </div>
+
+                <div className="buttons">
+                    <Button variant="outlined" startIcon={<AddOutlinedIcon />} onClick={handleOpenMaterialDialog}>Material</Button>
+                    <Button variant="outlined" startIcon={<AddOutlinedIcon />} onClick={handleOpenHomeworkDialog}>Homework</Button>
+                    <Button variant="outlined" startIcon={<AddOutlinedIcon />} onClick={handleOpenExamDialog}>Exam</Button>
+                    <Button variant="outlined" startIcon={<AddOutlinedIcon />} disabled>Update</Button>
+
+                    <NewMaterialDialog open={openMaterialDialog} setOpen={setOpenMaterialDialog}/>
+                    <NewHomeworkDialog open={openHomeworkDialog} setOpen={setOpenHomeworkDialog}/>
+                    <NewExamDialog open={openExamDialog} setOpen={setOpenExamDialog}/>
                 </div>
 
                 <div className="course">
