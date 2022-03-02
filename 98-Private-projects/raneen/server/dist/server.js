541ea2ae099db5db5eebf6269ad9fc85de6d96ff
raneenmandalawe <raneen.mandalawe@gmail.com>
2022-03-02 10:36:09 +0200

    ..

--- 98-Private-projects/raneen/server/dist/server.js
+++ 98-Private-projects/raneen/server/dist/server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,10 +15,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 const port = 4000;
+require('dotenv').config();
 app.use(express_1.default.static('../client/build'));
 app.get('/get-user', (req, res) => {
     console.log('user-request');
     res.status(202).send({ name: "Mohamad", id: 123 });
+});
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield mongoose.connect('mongodb://localhost:27017/test');
+    });
+}
+const kittySchema = new mongoose.Schema({
+    name: String
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- 98-Private-projects/raneen/server/server.ts
+++ 98-Private-projects/raneen/server/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 const port = 4000;
+require('dotenv').config();
 
 app.use(express.static('../client/build'));
 
@@ -8,6 +9,18 @@ app.get('/get-user',(req, res)=>{
     console.log('user-request')
     res.status(202).send({name:"Mohamad", id:123})
 })
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
