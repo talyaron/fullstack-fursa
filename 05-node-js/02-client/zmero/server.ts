c0c7de7e67894a9a67b5c996a9157a4396b2713e
mzmero <m.zmiro@hotmail.com>
2022-03-02 11:55:15 +0200

    mongo

--- 05-node-js/02-client/zmero/server.ts
+++ 05-node-js/02-client/zmero/server.ts
@@ -1,10 +1,52 @@
-declare function require(name: string);
-
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const port = 4000;
 
 app.use(express.static('client/build'));
+app.use(express.json());
+//data
+
+
+
+
+
+//posts
+
+
+
+
+
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+const db = mongoose.connection;
+
+
+async function main() {
+    const password = process.env.MONGODB_PASSWORD
+    await mongoose.connect(`mongodb+srv://zmero123:${password}@cluster0.acxtd.mongodb.net/test`)
+}
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("connected to DB!");
+});
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+//the collection
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const mitzy = new Kitten({ name: 'Mitzy' });
+//mitzy.save().then(res => { console.log(res) }).catch(err => console.log(err));
+async function getKitens() {
+    const kittens = await Kitten.find({ name: "Mitzy" });
+    console.log(kittens);
+}
+
+getKitens();
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- 98-Private-projects/zmero/server.ts
+++ 98-Private-projects/zmero/server.ts
@@ -270,13 +270,32 @@ app.post('/add-user-reservation', (req, res) => {
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
+const db = mongoose.connection;
 
-const password = process.env.MONGODB_PASSWORD
 
 async function main() {
+    const password = process.env.MONGODB_PASSWORD
     await mongoose.connect(`mongodb+srv://zmero123:${password}@cluster0.acxtd.mongodb.net/test`)
 }
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("connected to DB!");
+});
+const kittySchema = new mongoose.Schema({
+    name: String
+});
 
+//the collection
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const mitzy = new Kitten({ name: 'Mitzy' });
+//mitzy.save().then(res => { console.log(res) }).catch(err => console.log(err));
+async function getKitens() {
+    const kittens = await Kitten.find({ name: "Mitzy" });
+    console.log(kittens);
+}
+
+getKitens();
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
