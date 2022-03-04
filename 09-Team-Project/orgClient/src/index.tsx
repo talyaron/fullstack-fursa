d26a1e0bccb5c7f3a0a101679e2ad1778fa4296d
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-03-04 13:35:31 +0200

    add pages

--- 09-Team-Project/orgClient/src/index.tsx
+++ 09-Team-Project/orgClient/src/index.tsx
@@ -6,6 +6,8 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
---
+++ 09-Team-Project/orgClient/src/view/pages/accident/Accident.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Accident() {
+  return (
+    <div>Accident</div>
+  )
+}
+
+export default Accident
