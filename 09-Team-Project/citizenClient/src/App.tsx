51587a598cee8a410769e0faf4ccd0cc8a8b2dbf
mzmero <m.zmiro@hotmail.com>
2022-03-05 20:16:36 +0200

    navbar and home

--- 09-Team-Project/citizenClient/src/App.tsx
+++ 09-Team-Project/citizenClient/src/App.tsx
@@ -1,12 +1,13 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Home from './view/pages/home/home'
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ 09-Team-Project/citizenClient/src/view/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/navbar/navbar'
+
+function Home() {
+    return (
+        <div className="main">
+            <Navbar />
+            <div className="main__content">
+
+            </div>
+        </div>
+    )
+}
+export default Home
