4cbdd298e3ceca864d037fc5ce93ad2cd75e1560
mzmero <m.zmiro@hotmail.com>
2022-03-05 20:47:07 +0200

    home page

--- 09-Team-Project/citizenClient/src/view/pages/home/home.tsx
+++ 09-Team-Project/citizenClient/src/view/pages/home/home.tsx
@@ -1,12 +1,23 @@
+import Button from '@mui/material/Button'
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
+import CampaignIcon from '@mui/icons-material/Campaign';
+
+import './home.scss'
 
 function Home() {
     return (
-        <div className="main">
+        <div className="home">
             <Navbar />
-            <div className="main__content">
-
+            <div className="home__content">
+                <div className="home__content__btn">
+                    <Button style={{ backgroundColor: "#eb4034" }} variant="contained" startIcon={<CampaignIcon />}>
+                        Emergency
+                    </Button>
+                    <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">Harassment</Button>
+                    <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">Home Violence</Button>
+                    <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">violence event</Button>
+                </div>
             </div>
         </div>
     )
