6c3e6b1902370e08eff48dbd2d6cde4eee8811ae
AlaaAga <alaa.ali.aga1@gmail.com>
2022-03-06 13:26:36 +0200

    privateProject

--- 98-Private-projects/alaa-majd-app/Server/server.ts
+++ 98-Private-projects/alaa-majd-app/Server/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+const mongoose = require("mongoose");
+const products = require('./Routers/ProductRouter')
 const app = express();
 const port = 4001;
 
@@ -8,6 +10,15 @@ app.get("/", (req,res)=>{
     res.send("hello")
 })
 
+mongoose.connect('mongodb+srv://Alaa1:yEszVzboZEnwamwT@cluster0.es9yf.mongodb.net/test');
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("connected to DB!");
+});
+app.use("/product",products);
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
