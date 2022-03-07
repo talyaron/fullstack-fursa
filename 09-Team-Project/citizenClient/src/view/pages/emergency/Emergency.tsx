7d556dff479836e93b968e3c95da497dcbd31a04
Mona-1497 <96580971+Mona-1497@users.noreply.github.com>
2022-03-07 22:05:56 +0200

    update emergency

--- 09-Team-Project/citizenClient/src/view/pages/emergency/Emergency.tsx
+++ 09-Team-Project/citizenClient/src/view/pages/emergency/Emergency.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material'
 import './Emergency.scss'
 export function Emergency(){
     
@@ -5,16 +6,17 @@ export function Emergency(){
     return (
        <div className="emergency">
            <div className="emergency_header">
-               <h1>Emergency</h1>
+               <h1 >Emergency</h1>
+               </div>
                <div className="emergency_body">
-                   <button>video</button>
-                   <button>confirm location</button>
-                   <button>take a picture</button>
-                   <button>voice call</button>
-                   <button>report case</button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">video</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">location</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">picture</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">voice call</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">report case</Button>
 
 
-                   </div>
+                 
                </div>
            </div> 
     )
