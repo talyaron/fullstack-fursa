15183503462007997945dd3e6b9f7e13e1b4d4c3
Tal Yaron <tal.yaron@gmail.com>
2022-03-07 10:52:42 +0200

    cont

--- 09-Team-Project/server/model/accidentModel.ts
+++ 09-Team-Project/server/model/accidentModel.ts
@@ -1,17 +1,20 @@
 
-import userSchema from "./g5/userModel";
+import userSchema from "./userModel";
+import ImageSchema from './imageSchema';
 const mongoose = require("mongoose");
+
+
 const accidentSchema = new mongoose.Schema({
-    accidentId:Number,
-    accidentType: String,
-    emergency : String || null,
-    accidentDate: String,
-    accidentAddress:Location,
-    accidentPic: String,
-    accidentVideo:String,
-    voiceCall: Object,
-    accidentDescription: String,
-    accidentUser: userSchema,
+
+    type: String,
+    emergency : Boolean || null,
+    date: String,
+    address:Location,
+    pic: [],
+    video:String,
+    call: Object,
+    description: String,
+    user: userSchema,
 })
 const accident = mongoose.model("accident",accidentSchema);
 export default accidentSchema;
