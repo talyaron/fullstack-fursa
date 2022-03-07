2ecdd2b9bf9a78fc30ae1de7d4506fecacc687a4
lama.murad.lm@gmail.com <lama.murad.lm@gmail.com>
2022-03-07 18:36:39 +0200

    ..

--- 98-Private-projects/Lama2/server/server.ts
+++ 98-Private-projects/Lama2/server/server.ts
@@ -107,32 +107,32 @@ const kittySchema = new mongoose.Schema({
     res.send({kittens:kittens});
 })
 
-// app.get('/get-all-courses',async (req:any, res:any)=>{
-//   const courses = await getCourses();
-//   res.send({courses:courses});
-// })
+app.get('/get-all-courses',async (req:any, res:any)=>{
+  const courses = await getCourses();
+  res.send({courses:courses});
+})
 
 
-// app.post("/add-new-course", async (req, res) => {
-//   try {
-//     const { name, cost, participants,lessons,hours } = req.body;
-//     if (!name || !cost || !participants || !lessons || !hours) throw new Error("No data");
-//     console.log(name);
-//     const newCourse = new Course({
-//       name: name,
-//       cost: cost,
-//       participants: participants,
-//       lessons:lessons,
-//       hours:hours,
-//     });
-//     await newCourse.save().then((res) => {
-//       console.log(res);
-//     });
-//     res.send({ val: "OK" });
-//   } catch (err) {
-//     res.send({ error: err.message });
-//   }
-// });
+app.post("/add-new-course", async (req, res) => {
+  try {
+    const { name, cost, participants,lessons,hours } = req.body;
+    if (!name || !cost || !participants || !lessons || !hours) throw new Error("No data");
+    console.log(name);
+    const newCourse = new Course({
+      name: name,
+      cost: cost,
+      participants: participants,
+      lessons:lessons,
+      hours:hours,
+    });
+    await newCourse.save().then((res) => {
+      console.log(res);
+    });
+    res.send({ val: "OK" });
+  } catch (err) {
+    res.send({ error: err.message });
+  }
+});
 
 
 app.use(bodyParser.json());
