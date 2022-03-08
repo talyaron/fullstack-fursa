9c3f53c77f7602eb3d9fc9b670f73a4765df57d4
Mona-1497 <96580971+Mona-1497@users.noreply.github.com>
2022-03-08 15:16:03 +0200

    update

--- 09-Team-Project/citizenClient/src/view/pages/emergency/Emergency.tsx
+++ 09-Team-Project/citizenClient/src/view/pages/emergency/Emergency.tsx
@@ -6,9 +6,10 @@ export function Emergency(){
     return (
        <div className="emergency">
            <div className="emergency_header">
-               <h1 >Emergency</h1>
+               <h1 >Accident</h1>
                </div>
                <div className="emergency_body">
+                   <input type='image' ></input>
                <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">video</Button>
                <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">location</Button>
                <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">picture</Button>
