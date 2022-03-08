db5091d5b13c31eb5a6726cc34054ed60185aff3
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-03-09 00:14:06 +0200

    add accidents

--- 09-Team-Project/orgClient/src/app/store.ts
+++ 09-Team-Project/orgClient/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import AccidentsReducer from '../features/accidents/accidentsReducer';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    accidents: AccidentsReducer,
   },
 });
 
