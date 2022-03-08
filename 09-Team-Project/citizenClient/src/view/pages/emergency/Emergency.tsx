b837e19d8de06adf02e55a2a9b200257d7fba4d7
Mona-1497 <96580971+Mona-1497@users.noreply.github.com>
2022-03-08 15:30:42 +0200

    update

--- 09-Team-Project/citizenClient/src/view/pages/emergency/Emergency.tsx
+++ 09-Team-Project/citizenClient/src/view/pages/emergency/Emergency.tsx
@@ -10,11 +10,13 @@ export function Emergency(){
                </div>
                <div className="emergency_body">
                    <input type='image' ></input>
-               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">video</Button>
-               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">location</Button>
-               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">picture</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">confirm location</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">upload video</Button>
+               <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">upload picture</Button>
                <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">voice call</Button>
                <Button style={{ borderColor: "#eb4034", color: "#eb655b" }} variant="outlined">report case</Button>
+               <input type='text' placeholder='accident description'></input>
+              
 
 
                  
