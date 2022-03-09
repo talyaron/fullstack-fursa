d059fb72d2b1696f93d9c077d8354bcca2725e16
AlaaAga <alaa.ali.aga1@gmail.com>
2022-03-09 13:00:29 +0200

    ...

--- 98-Private-projects/alaa-majd-app/Server/server.ts
+++ 98-Private-projects/alaa-majd-app/Server/server.ts
@@ -12,6 +12,20 @@ app.get("/", (req,res)=>{
 
 mongoose.connect('mongodb+srv://Alaa1:yEszVzboZEnwamwT@cluster0.es9yf.mongodb.net/test');
 const db = mongoose.connection;
+const userSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+});
+
+async function main() {
+  const password = process.env.MONGODB_PASSWORD;
+  await mongoose.connect(`mongodb+srv://Alaa1:yEszVzboZEnwamwT@cluster0.es9yf.mongodb.net/priviteProjectDatabase?retryWrites=true&w=majority`);
+}
+
+const User = mongoose.model('appointment', userSchema);
+const event = new User({ name: 'asma', password:"123" });
+console.log(event.name);
+event.save();
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
