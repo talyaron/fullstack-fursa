93b18e058abc6cf4ce8a8e5f701023c13f8b5cac
Majdna <48679257+Majdna@users.noreply.github.com>
2022-03-09 20:49:35 +0200

    update-getAccidentsByLocation

--- 09-Team-Project/orgClient/src/view/pages/accidentsByLocation/accidentsByLocation.tsx
+++ 09-Team-Project/orgClient/src/view/pages/accidentsByLocation/accidentsByLocation.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 function AccidenstByLocation() {
   
   interface Accident{
+  _id:string,
   type: string,
   emergency : boolean,
   date: string,
@@ -16,23 +17,28 @@ function AccidenstByLocation() {
 
   async function handleSearchByLocation(e:any){
     e.preventDefault();
-    const a:any=await axios.post("/accidents/getAccidenstByLocation", {location:location});
-    const v:any= await axios.get("/accidents/getAllAccidents");
+    let a:any=await axios.post("/accidents/getAccidenstByLocation", {location:location});
+    //const v:any= await axios.get("/accidents/getAllAccidents");
     console.log(a);
-    setAccidents(a);
+    console.log(a.data.accidents);
+    setAccidents(a.data.accidents);
   }
-
   return (
    <div>
     <input type="text" placeholder='Enter Location' onKeyUp={(e:any)=>{setLocation(e.target.value)}} />
     <button onClick={handleSearchByLocation}> search</button>
-    
-    {accidents.map((acc) => (
-             <div>
-            <div>{acc.emergency}</div>
-            <div>{acc.date}</div>
+     
+    <div>
+
+    {accidents.map((accident, index) => {
+          return (
+            <div key={index}>
+            <p >id: {accident._id} type:{accident.type} date:{accident.date} description:{accident.description}  </p> 
             </div>
-          ))}
+          );
+        })}
+
+     </div>
 
         {/* {accidents.map((acc, index) => {
          
@@ -45,7 +51,7 @@ function AccidenstByLocation() {
         })} */}
    </div>
 
-  )
+  );
 
 }
 
