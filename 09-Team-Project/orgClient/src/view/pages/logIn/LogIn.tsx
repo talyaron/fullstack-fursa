b0806e35c6fbbd82b9452e1a3e670e2d0ddeef78
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-03-09 10:52:09 +0200

    add orgUserReducer

--- 09-Team-Project/orgClient/src/view/pages/logIn/LogIn.tsx
+++ 09-Team-Project/orgClient/src/view/pages/logIn/LogIn.tsx
@@ -3,26 +3,22 @@ import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { useAppDispatch } from '../../../app/hooks';
 import { AnyArray } from 'immer/dist/internal';
-import {useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { fetchOrgUserAsync } from '../../../features/orgUser/orgUserReducer';
 
 
 
 function Login() {
     const [ID, setID] = useState("");
     const [password, setPassword] = useState("");
-    const nav=useNavigate();
+    const nav = useNavigate();
     const dispatch = useAppDispatch();
 
     function hadleSubmit(e: any) {
         e.preventDefault();
 
-        // dispatch(login([ID, password, true]));
-            if(ID === "314763467" && password==="123456")
-            {  nav("/Seller");}
-             else if(ID !== "" && password!=="")
-              {
-                nav("/Group");} 
-             }
+        dispatch(fetchOrgUserAsync({ email: ID, password: password }));
+    }
 
 
     return (
@@ -32,10 +28,10 @@ function Login() {
                     Login <br /><br />
                     <br /><br />
                     ID <br />
-                    <input type="text" onKeyUp={(e:any)=>{setID(e.target.value)}} />
+                    <input type="text" onKeyUp={(e: any) => { setID(e.target.value) }} />
                     <br /><br />
                     Password <br />
-                    <input type="password" onKeyUp={(e:any)=>{setPassword(e.target.value)}} />
+                    <input type="password" onKeyUp={(e: any) => { setPassword(e.target.value) }} />
                     <br /><br />
                     <input type='submit' className='submit' value="Login" />
                     <br />
