fb34bdaabe87f0c95e1408a11b79890fafc637c0
raneenmandalawe <raneen.mandalawe@gmail.com>
2022-03-10 10:34:09 +0200

    privateInfo

--- 09-Team-Project/orgClient/src/view/pages/profileInfo/ProfileInfo.tsx
+++ 09-Team-Project/orgClient/src/view/pages/profileInfo/ProfileInfo.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import './style.scss'
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { accident, findAccidentAsync, removeSelected } from '../../../app/reducer/accidentInfoReducer';
 
 function ProfileInfo() {
-
-  const accident_ = useAppSelector(state => state.accident);
-    const info ={name:"Name",city:"some city",phone:" 0527-***-***"}
+// interface userIF {
+//     userID?: Number,
+//     name?: String,
+//     email?: String,
+//     password?: String,
+//     phone?: String,
+//     location?: String,
+//     gender?: String,
+//     type?: String,
+//     org?: orgIF
+// }
+  const user = useAppSelector(accident).value.user;
 
     return (
-        <div>
+        <div className='wrapper'>
+      <div className='menu'>
+        <HomeIcon sx={{ paddingLeft: '20px', fontSize: 35, paddingTop: '10px' }} />
+        <SettingsIcon sx={{ float: 'right', fontSize: 35, paddingRight: '20px', paddingTop: '10px' }} />
+      </div>
+      
             <div className="profileInfo">
-                <h1>{info.name}</h1>
-                <div><p>City: {info.city}</p></div>
-                <div><p>Phone Number: {info.phone}</p></div>
+                <h1>{user.name}</h1>
+                <div><p>EMAIL: {user.email}</p></div>
+                  <div><p>PHONE NUMBER: {user.phone}</p></div>
+                <div><p>LOCATION: {user.location}</p></div>
+                  <div><p>GENDER: {user.gender}</p></div>
+                             <div><p>TYPE: {user.type}</p></div>
             </div >
         </div>
     )
