5026615fade00eadfc5853b1068eec338af66899
Tal Yaron <tal.yaron@gmail.com>
2022-03-13 15:05:19 +0200

    בםמא

--- 07-mySQL/01-basic/Tal/server/routes/userRoute.ts
+++ 07-mySQL/01-basic/Tal/server/routes/userRoute.ts
@@ -1,65 +1,7 @@
 const router = require("express").Router();
 import { con } from "../server";
+import { register, login } from "./userControls";
 
-router.post("/register", (req, res) => {
-  try {
-    const { name, city, password } = req.body;
-    if (!name) throw new Error("No name in body");
-    if (!city) throw new Error("No city in body");
-    if (!password) throw new Error("No password in body");
+router.post("/register", register).post("/login", login);
 
-    console.log("connect to rgister route");
-    const sql = `INSERT INTO cats.owners(id,name,city,password) VALUES (${uid()},'${name}','${city}', ${password});`;
-    con.query(sql, (err, result) => {
-      try {
-        if (err) throw err;
-        console.log("1 record inserted");
-        res.send({ results: result });
-      } catch (err) {
-        console.log(err.message);
-        res.send({ error: err.message });
-      }
-    });
-  } catch (error) {
-    console.log(`In post-registe error: ${error.message}`);
-    res.status(500).send({ error: error.message });
-  }
-});
-
-router.post("/login", (req, res) => {
-  try {
-    const { name, password } = req.body;
-    if (!name) throw new Error("No name in body");
-    if (!password) throw new Error("No password in body");
-
-    console.log("connect to login route");
-    const sql = `SELECT * FROM cats.owners WHERE name = '${name}';`;
-    con.query(sql, (err, result) => {
-      try {
-        if (err) throw err;
-        console.log("1 record inserted");
-        if (result.length > 0) {
-          const passwordDB = result[0].password;
-          if (password === passwordDB) {
-            res.send({ login: true, userId: result[0].id });
-          } else {
-            res.send({ error: "no user with such name or password" });
-          }
-        } else {
-          res.send({ error: "no user with such name or password" });
-        }
-      } catch (err) {
-        console.log(err.message);
-        res.send({ error: err.message });
-      }
-    });
-  } catch (error) {
-    console.log(`In post-registe error: ${error.message}`);
-    res.status(500).send({ error: error.message });
-  }
-});
-
-function uid() {
-  return Math.floor(Math.random() * 1e6);
-}
 export default router;
