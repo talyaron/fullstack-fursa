11c9ec35be38885c9c2ae54d3c5a1da74cf2910c
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-03-14 12:41:09 +0200

    add bar style

--- 09-Team-Project/orgClient/src/view/Components/header/appBar.tsx
+++ 09-Team-Project/orgClient/src/view/Components/header/appBar.tsx
@@ -4,23 +4,47 @@ import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useNavigate } from 'react-router-dom';
+import ChatIcon from '@mui/icons-material/Chat';
+import SearchIcon from '@mui/icons-material/Search';
 
-export default function MenuAppBar() {
+export default function MenuAppBar(props: any) {
   const nav = useNavigate();
+  const { type } = props;
 
   return (
     <AppBar position="static" style={{ backgroundColor: "#889FEF" }}>
       <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <HomeIcon onClick={() => nav('/accidents')} style={{ fontSize: "40px" }}  />
-        </IconButton>
-
+        {type !== "home" ?
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <HomeIcon onClick={() => nav('/accidents')} style={{ fontSize: "40px" }} />
+          </IconButton> :
+          <>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <ChatIcon onClick={() => nav('/accidents')} style={{ fontSize: "40px" }} />
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <SearchIcon onClick={() => nav('/accidents')} style={{ fontSize: "40px" }} />
+            </IconButton>
+          </>
+        }
         <IconButton
           size="large"
           edge="end"
