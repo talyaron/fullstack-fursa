8d28732671860c46ea15cb75b20a9cf642df9b68
mzmero <m.zmiro@hotmail.com>
2022-03-14 09:08:06 +0200

    ..

--- 05-node-js/Express/01-basic/zmero/public/js/index.ts
+++
@@ -1,8 +0,0 @@
-console.log("hi all")
-
-async function init() {
-    console.log("init")
-    const response = await fetch('/get-user');
-    const user = await response.json();
-    console.log(user)
-}
