8e8a56b05173b9926eeebb818c9b735293f5075a
mzmero <m.zmiro@hotmail.com>
2022-03-14 10:07:21 +0200

    fix server

--- 09-Team-Project/server/server.ts
+++ 09-Team-Project/server/server.ts
@@ -18,36 +18,26 @@ require('dotenv').config();
 
 
 mongoose.connect(`mongodb+srv://${process.env.ATLAS_NAME}:${process.env.CLUSTER_PASS}@cluster0.qqi5o.mongodb.net/test`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("connected to DB!");
+  console.log("connected to DB!");
 });
 
-const io = require('socket.io')(http)
 
-io.of('/api/socket').on('connection', socket => {
-    console.log('A user is connected');
-    socket.on('message', ({ name, message }) => {
-        io.emit('message', { name, message })
-    })
 
-    socket.on('disconnect', () => {
-        console.log(`socket ${socket.id} disconnected`);
-    })
-})
+
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 })
 
 const accidentRouter = require('./routers/accidentRouter');
-const userRouter = require('./routers/userRouter')
 app.use('/accidents', accidentRouter);
 
 const userRouter = require('./routers/userRouter');
@@ -61,7 +51,7 @@ const messagesBetweemOrgRouter = require('./routers/messagesBetweemOrgRouter');
 app.use('/messagesBetweemOrg', messagesBetweemOrgRouter);
 
 server.listen(port, () => {
-    console.log(`Listening on port ${port} 🔥`)
+  console.log(`Listening on port ${port} 🔥`)
 })
 
 
@@ -69,15 +59,15 @@ server.listen(port, () => {
 
 // saleem part
 const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-  });
+  cors: {
+    origin: '*',
+  }
+});
 let interval;
 let userId;
 let orgId;
 io.on("connection", (socket) => {
-    console.log("new client join socket")
+  console.log("new client join socket")
   if (interval) {
     clearInterval(interval);
   }
@@ -89,14 +79,14 @@ io.on("connection", (socket) => {
   socket.on('message', (value) => handleMessage(value));
   socket.on("setUserData", userData => {
     //When user creation on server is complete, retrieve and save data to local storage
-          console.log("user id is "+ JSON.stringify( userData))
-          userId = userData;
- });
- socket.on("setOrgData", userData => {
+    console.log("user id is " + JSON.stringify(userData))
+    userId = userData;
+  });
+  socket.on("setOrgData", userData => {
     //When user creation on server is complete, retrieve and save data to local storage
-          console.log("org id is "+ userData)
-          orgId = orgId
- });
+    console.log("org id is " + userData)
+    orgId = orgId
+  });
 });
 
 const getApiAndEmit = socket => {
@@ -104,9 +94,9 @@ const getApiAndEmit = socket => {
   // Emitting a new message. Will be consumed by the client
   socket.emit("FromAPI", response);
 };
-  
+
 
 function handleMessage(value: any) {
-    console.log(value);
+  console.log(value);
 }
 /* end of saleem */
