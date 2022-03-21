bd07461dcdbc46262ffabf10048259d3e78015a3
mzmero <m.zmiro@hotmail.com>
2022-03-21 11:08:32 +0200

    fix query

--- 09-Team-Project/server/controllers/previousAccidentController.ts
+++ 09-Team-Project/server/controllers/previousAccidentController.ts
@@ -5,8 +5,7 @@ exports.getPreviousAccidents = async (req, res) => {
     console.log('get-previous-accidents');
     try {
         const { email } = req.body;
-        console.log(email)
-        const _acc = await accidents.find({ user: { email: email } });
+        const _acc = await accidents.find({ "user.email": email });
         res.send({ ok: true, accident: _acc });
     } catch (error: any) {
         res.send({ ok: false, error: error.message });
