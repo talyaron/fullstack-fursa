870d1de8684fac0b8f6cc2ff7ba94f068880c135
saleem <sbsaleem76@gmail.com>
2022-03-23 11:17:40 +0200

    design done

--- 09-Team-Project/citizenClient/src/view/pages/ChatRooms/chatRooms.tsx
+++ 09-Team-Project/citizenClient/src/view/pages/ChatRooms/chatRooms.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import './chatRooms.scss'
 
+
+interface roomFormat{
+  chatRoomId: String,
+  user1:String,
+  user2:String
+}
+
 function ChatRooms(){
+
+  const [rooms,setRooms] = useState([]);
+
   return (
+    <div className="chatRoomsWrapper">
     <div className="box">
   <div className="container-4">
     <input type="search" id="search" placeholder="Search..." />
     <button className="icon"><i className="fa fa-search"></i></button>
   </div>
-</div>)
+</div>
+<div className="rooms">
+  {
+      rooms.map((elemnt:roomFormat, index) => {
+        return (
+          <h1>{elemnt.user2}</h1>
+        )})}
+
+
+    
+</div>
+</div>
+
+
+)
+
 }
 export default ChatRooms;
