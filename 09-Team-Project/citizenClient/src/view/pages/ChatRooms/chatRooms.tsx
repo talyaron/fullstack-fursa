ac14df8ea1ad9ff018fbab25234bec61da74b761
saleem <sbsaleem76@gmail.com>
2022-03-23 12:21:43 +0200

    for merge

--- 09-Team-Project/citizenClient/src/view/pages/ChatRooms/chatRooms.tsx
+++ 09-Team-Project/citizenClient/src/view/pages/ChatRooms/chatRooms.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import socketIOClient,{ Socket } from "socket.io-client";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import './chatRooms.scss'
+const ENDPOINT = "http://localhost:3001/";
+let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 
+const user1 = "1";
+const user2 = "2"
 
 interface roomFormat{
   chatRoomId: String,
@@ -10,8 +16,16 @@ interface roomFormat{
 
 function ChatRooms(){
 
-  const [rooms,setRooms] = useState([]);
-
+  const [rooms,setRooms] = useState({});
+  useEffect(() => {
+    socket = socketIOClient(ENDPOINT);
+    socket.on("getRoams", (data: React.SetStateAction<string>) => {
+      setRooms(data);
+    });
+    /* set user id */
+    socket.emit("setUserData", user1);
+    
+  }, []);
   return (
     <div className="chatRoomsWrapper">
     <div className="box">
@@ -21,11 +35,12 @@ function ChatRooms(){
   </div>
 </div>
 <div className="rooms">
-  {
+ {/*
       rooms.map((elemnt:roomFormat, index) => {
         return (
           <h1>{elemnt.user2}</h1>
-        )})}
+        )})*/
+}
 
 
     
